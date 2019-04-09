5c1809dbb7e5731fb330dca06c68946c17008518
Donald <448641125@qq.com>
2019-04-10 01:59:41 +0800

    add leetcode 5

---
+++ src/main/java/chapter1/topic1/LongestPalindromicSubstring.java
@@ -0,0 +1,79 @@
+package chapter1.topic1;
+
+/**
+ * LeetCode 5
+ *
+ *
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ *
+ *
+ */
+public class LongestPalindromicSubstring {
+
+    public static void main(String[] args) {
+
+        LongestPalindromicSubstring longestPalindromicSubstring = new LongestPalindromicSubstring();
+
+        System.out.println(longestPalindromicSubstring.longestPalindrome(""));
+
+        /*char [] arr = "bccb".toCharArray();
+
+        System.out.println(longestPalindromicSubstring.isPalindromic(arr, 0 ,3));*/
+    }
+
+    public String longestPalindrome(String s) {
+
+        if (s.length() == 0) {
+
+            return "";
+        }
+
+        char [] arr = s.toCharArray();
+
+        int length = 0;
+        int start = 0;
+        int end = 0;
+
+        for (int i = 0 ; i < arr.length - 1; ++i) {
+
+            for (int j = arr.length - 1; j > 0; --j) {
+
+                if (isPalindromic(arr, i, j)) {
+
+                    if (length < (j - i + 1)) {
+
+                        length = j - i + 1;
+                        start = i;
+                        end = j;
+                    }
+                }
+
+            }
+        }
+
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = start; i <= end; ++i) {
+
+            stringBuilder.append(arr[i]);
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private boolean isPalindromic(char[] arr, int start, int end) {
+
+        int len = (end - start + 1) / 2;
+
+        for (int i = start, j = end; i < start + len && j > end - len; ++i, --j) {
+
+            if (arr[i] != arr[j]) {
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
