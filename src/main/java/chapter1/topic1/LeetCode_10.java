48239f8bc6c04783e649e99b9c0acfd12cc7fe1a
Donald <448641125@qq.com>
2019-05-01 23:52:31 +0800

    update leetcode 10

--- src/main/java/chapter1/topic1/LeetCode_10.java
+++ src/main/java/chapter1/topic1/LeetCode_10.java
@@ -1,6 +1,73 @@
 package chapter1.topic1;
 
+/**
+ *  Regular Expression Matching
+ *
+ * Input:
+ * s = "aa"
+ * p = "a"
+ * Output: false
+ *
+ * Input:
+ * s = "aa"
+ * p = "a*"
+ * Output: true
+ *
+ * Input:
+ * s = "ab"
+ * p = ".*"
+ * Output: true
+ *
+ * means：p=".."
+ *
+ * Input:
+ * s = "mississippi"
+ * p = "mis*is*p*."
+ * Output: false
+ *
+ * Input:
+ * s = "sssss"
+ * p = "ss*s"
+ * Output: false
+ *
+ * Input:
+ * s = "ssssss"
+ * p = "ss*s.*"
+ * Output: false
+ *
+ * '.' Matches any single character.
+ * '*' Matches zero or more of the preceding element.
+ */
 public class LeetCode_10 {
 
+    public boolean isMatch(String s, String p) {
 
+        for (int i = 0; i < s.length(); ++i) {
+
+            // 若 s 字符串 大于 p 字符串
+            if (i >= p.length()) {
+
+                return false;
+            }
+
+            char pChar = p.charAt(i);
+
+            if (pChar == '.') {
+
+                continue;
+            }
+
+            // 如何判断是否需要 *, 0 or more
+            if (pChar == '*') {
+
+                if (i > 0) {
+
+                    s.charAt(i);
+                }
+
+            }
+        }
+
+        return true;
+    }
 }
