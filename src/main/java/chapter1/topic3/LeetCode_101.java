8cc55be045ff2571014e2e04a8ddfa89fde56c1b
Donald <448641125@qq.com>
2019-05-07 12:32:21 +0800

    update leetcode 101

--- src/main/java/chapter1/topic3/LeetCode_101.java
+++ src/main/java/chapter1/topic3/LeetCode_101.java
@@ -18,14 +18,11 @@ package chapter1.topic3;
  *   2   2
  *    \   \
  *    3    3
- */
 
-import java.util.ArrayList;
-import java.util.List;
-
-/**
  * 题意： 判断二叉树是否对称
  *
+ * 1. 每一层进行比较（回文比较）
+ *    BFS 可以拿到按层级拿取节点
  */
 
 class TreeNode {
@@ -37,27 +34,66 @@ class TreeNode {
 
 public class LeetCode_101 {
 
+    public static void main(String[] args) {
+
+        TreeNode tree1 = initTree1();
+        TreeNode tree2 = initTree2();
+
+        LeetCode_101 leetCode = new LeetCode_101();
+        System.out.println(leetCode.isSymmetric(tree1));
+
+        System.out.println(leetCode.isSymmetric(tree2));
+    }
+
+    private static TreeNode initTree1() {
+
+        TreeNode node31 = getNode(3, null, null);
+        TreeNode node32 = getNode(4, null, null);
+        TreeNode node33 = getNode(4, null, null);
+        TreeNode node34 = getNode(3, null, null);
+        TreeNode node21 = getNode(2, node31, node32);
+        TreeNode node22 = getNode(2, node33, node34);
+
+        return getNode(1, node21, node22);
+    }
+
+    private static TreeNode initTree2() {
+
+        TreeNode node32 = getNode(4, null, null);
+        TreeNode node34 = getNode(3, null, null);
+        TreeNode node21 = getNode(2, null, node32);
+        TreeNode node22 = getNode(2, null, node34);
+
+        return getNode(1, node21, node22);
+    }
+
+    private static TreeNode getNode(int val, TreeNode node1, TreeNode node2) {
+
+        TreeNode node = new TreeNode(val);
+        node.left = node1;
+        node.right = node2;
+
+        return node;
+    }
+
+    // Time o(n), Space o(n)
     public boolean isSymmetric(TreeNode root) {
-
-        List<Integer> values = new ArrayList<Integer>();
-
-        TreeNode node = root;
-
-        while (node != null) {
-
-            values.add(node.val);
-
-            if (node.left != null) {
-
-                values.add(node.left.val);
-            }
-
-            if (node.right != null) {
-
-                values.add(node.right.val);
-            }
+        if (null == root) {
+            return true;
         }
 
-        return true;
+        return isSymmetricNode(root.left, root.right);
+    }
+
+    private boolean isSymmetricNode(TreeNode left, TreeNode right) {
+
+        if (left != null && right != null) {
+
+            if (left.val != right.val) return false;
+
+            return isSymmetricNode(left.left, right.right) && isSymmetricNode(left.right , right.left);
+        }
+
+        return left == null && right == null;
     }
 }
