91d4a446ce55fec542d68f0fb1a1c3796ab4e42f
Donald <448641125@qq.com>
2019-05-09 20:17:53 +0800

    update leetcode 50

--- src/main/java/chapter1/topic1/LeetCode_50.java
+++ src/main/java/chapter1/topic1/LeetCode_50.java
@@ -12,6 +12,12 @@ package chapter1.topic1;
  * Input: 2.00000, -2
  * Output: 0.25000
  *
+ * 题意：计算某一个数的次方
+ *
+ * 思路：
+ * 1. 调API
+ * 2. 手动计算，循环相乘
+ * 3. 位运算 x^11 = x^8 x^2 X^1
  *
  */
 public class LeetCode_50 {
@@ -29,5 +35,28 @@ public class LeetCode_50 {
     public double myPow(double x, int n) {
 
         return Math.pow(x, n);
+    }
+
+    // Time: O(n), Space: O(1)
+    public double pow(double x, int n) {
+        double result = 1;
+        long N = Math.abs((long)n);
+
+        for (int i = 0; i < N; ++i)
+            result *= x;
+        return n < 0 ? 1/result : result;
+    }
+
+    // Time: O(log(n)), Space: O(1)
+    public double powFast(double x, int n) {
+        double result = 1;
+        long N = Math.abs((long)n);
+
+        while (N != 0) {
+            if ((N & 1) == 1) result *= x;
+            x *= x;
+            N >>= 1;
+        }
+        return n < 0 ? 1/result : result;
     }
 }
