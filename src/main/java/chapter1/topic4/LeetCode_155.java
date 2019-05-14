a7e12104e8c5fb793f1c6006646a730b2e4c3080
Donald <448641125@qq.com>
2019-05-14 23:42:45 +0800

    add leetcode 155

---
+++ src/main/java/chapter1/topic4/LeetCode_155.java
@@ -0,0 +1,52 @@
+package chapter1.topic4;
+
+import java.util.Stack;
+
+/**
+ * 155. Min Stack
+ *
+ * MinStack minStack = new MinStack();
+ * minStack.push(-2);
+ * minStack.push(0);
+ * minStack.push(-3);
+ * minStack.getMin();   --> Returns -3.
+ * minStack.pop();
+ * minStack.top();      --> Returns 0.
+ * minStack.getMin();   --> Returns -2.
+ */
+public class LeetCode_155 {
+
+    public static void main(String[] args) {
+
+        MinStack obj = new MinStack();
+        obj.push(1);
+        obj.pop();
+        int param_3 = obj.top();
+        int param_4 = obj.getMin();
+    }
+
+}
+
+class MinStack {
+
+    private Stack<Integer> stack = new Stack<>();
+    private Stack<Integer> min = new Stack<>();
+
+    public void push(int x) {
+        stack.push(x);
+        if (min.isEmpty() || x <= getMin()) min.push(x);
+    }
+
+    public void pop() {
+        if (stack.peek() == getMin()) min.pop();
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min.peek();
+    }
+}
