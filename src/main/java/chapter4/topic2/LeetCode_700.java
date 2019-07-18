0b3dea5e7b066e26eabb4063b4a644b8945dfa0a
Donald <448641125@qq.com>
2019-07-19 00:29:16 +0800

    add leetcode 700

---
+++ src/main/java/chapter4/topic2/LeetCode_700.java
@@ -0,0 +1,55 @@
+package chapter4.topic2;
+
+/**
+ * 700. Search in a Binary Search Tree
+ *
+ * Given the tree:
+ *         4
+ *        / \
+ *       2   7
+ *      / \
+ *     1   3
+ *
+ * And the value to search: 2
+ * You should return this subtree:
+ *
+ *       2
+ *      / \
+ *     1   3
+ *
+ * 二叉搜索树的定义是：
+ *
+ *   1. 左子树所有节点上的值都小于根节点上的值
+ *   2. 右子树所有节点上的值都大于根节点上的值
+ *   3. 左右子树同时也为二叉搜索树
+ *
+ * 题意：在二叉搜索树中查找对应的值
+ *
+ * 思路：递归查找
+ */
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class LeetCode_700 {
+
+    public TreeNode searchBST(TreeNode root, int val) {
+
+        if (root == null) return null;
+
+        int value = root.val;
+
+        if (value == val) return root;
+
+        if (val < value) {
+
+            return searchBST(root.left, val);
+        } else {
+            return searchBST(root.right, val);
+        }
+    }
+}
