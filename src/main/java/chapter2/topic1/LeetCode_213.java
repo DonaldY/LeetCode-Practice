d944fb269f9ed179d61eec5f0099604b45c74dda
Donald <448641125@qq.com>
2019-07-24 01:21:13 +0800

    add leetcode 213

---
+++ src/main/java/chapter2/topic1/LeetCode_213.java
@@ -0,0 +1,68 @@
+package chapter2.topic1;
+
+import java.util.Arrays;
+
+/**
+ * 213. House Robber II
+ *
+ * Example 1:
+ *
+ * Input: [2,3,2]
+ * Output: 3
+ * Explanation: You cannot rob house 1 (money = 2) and then rob house 3 (money = 2),
+ *              because they are adjacent houses.
+ * Example 2:
+ *
+ * Input: [1,2,3,1]
+ * Output: 4
+ * Explanation: Rob house 1 (money = 1) and then rob house 3 (money = 3).
+ *              Total amount you can rob = 1 + 3 = 4.
+ *
+ * 题意：
+ * 1. 求间隔数字之间最大值， 首尾是相连的
+ *
+ * 思路：
+ * 1. 按照之前 leetcode 198思路
+ *    然后去掉尾，抢一次
+ *    然后去掉首，抢一次
+ *    取这两次的最大值
+ *
+ */
+public class LeetCode_213 {
+
+    // Time: o(n), Space: o(n), Faster: 100.00%
+    public int rob(int[] nums) {
+
+        if (nums == null || nums.length == 0) return 0;
+
+        if (nums.length == 1) return nums[0];
+
+        int withoutTailResult = this.robInternal(Arrays.copyOfRange(nums, 0, nums.length - 1));
+
+        int withoutHeadResult = this.robInternal(Arrays.copyOfRange(nums,1 , nums.length));
+
+        return Math.max(withoutHeadResult, withoutTailResult);
+    }
+
+    private int robInternal(int[] nums) {
+
+        if (nums == null || nums.length == 0) return 0;
+
+        if (nums.length == 1) return nums[0];
+
+        int d1 = nums[0];
+
+        int d2 = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; ++i) {
+
+            int temp = d2;
+
+            d2 = Math.max(d1 + nums[i], d2);
+
+            d1 = temp;
+        }
+
+        return d2;
+    }
+}
