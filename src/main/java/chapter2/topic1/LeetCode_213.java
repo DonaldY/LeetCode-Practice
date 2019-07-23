6484606b9d7d96808e12af58e6f7d48c30362e86
Donald <448641125@qq.com>
2019-07-24 01:22:16 +0800

    update leetcode 213

--- src/main/java/chapter2/topic1/LeetCode_213.java
+++ src/main/java/chapter2/topic1/LeetCode_213.java
@@ -65,4 +65,24 @@ public class LeetCode_213 {
 
         return d2;
     }
+
+
+    private int rob(int[] nums, int start, int end) {
+        int prev2 = 0, prev1 = 0;
+        for (int i = start; i <= end; ++i) {
+            int cur = Math.max(prev1, prev2+nums[i]);
+            prev2 = prev1;
+            prev1 = cur;
+        }
+        return prev1;
+    }
+
+    // Time: O(n), Space: O(1)
+    public int rob2(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        return Math.max(rob(nums, 0, n-2), rob(nums, 1, n-1));
+    }
+
 }
