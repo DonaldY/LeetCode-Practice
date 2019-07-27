7bd8911aeede6d2803daa9b3f34d4ad165130c7e
Donald <448641125@qq.com>
2019-07-27 11:20:02 +0800

    update leetcode 62

--- src/main/java/chapter1/topic2/LeetCode_62.java
+++ src/main/java/chapter1/topic2/LeetCode_62.java
@@ -24,6 +24,12 @@ package chapter1.topic2;
  * 思路：
  * 1. DFS，递归方式
  * 2. 动态规划题
+ *    d(0, j) = 1
+ *    d(i, 0) = 1
+ *    d(i, j) = d(i - 1) + d(i, j - 1)
+ *
+ * 3. 排列组合题
+ *
  */
 public class LeetCode_62 {
 
@@ -54,6 +60,37 @@ public class LeetCode_62 {
 
         findStar(col + 1, row, m, n);
         findStar(col, row + 1, m, n);
+    }
+
+    // Time: o(m*n), Space: o(m*n), Faster: 100.00%
+    public int uniquePathsDP(int m, int n) {
+
+        if (m < 1 || n < 1) return 0;
+        int [][] d = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            d[i][0] = 1;
+        }
+        for (int j = 0; j < n; ++j) {
+            d[0][j] = 1;
+        }
+
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                d[i][j] = d[i -1][j] + d[i][j -1];
+            }
+        }
+        return d[m-1][n-1];
+    }
+
+    // Time: O(min(m, n)), Space: O(1)
+    public int uniquePathsMath(int m, int n) {
+        if (m < 1 || n < 1) return 0;
+        int small = Math.min(m-1, n-1);
+        int total = m + n - 2;
+        long result = 1;
+        for (int i = 0; i < small; ++i)
+            result = result * (total-i) / (i+1);
+        return (int)result;
     }
 
 }
