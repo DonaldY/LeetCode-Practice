47a0ae2c50a9b0a47414334d5a25bb009b764e0e
Donald <448641125@qq.com>
2019-08-01 20:01:49 +0800

    update leetcode 617

--- src/main/java/chapter4/topic1/LeetCode_617.java
+++ src/main/java/chapter4/topic1/LeetCode_617.java
@@ -58,4 +58,15 @@ public class LeetCode_617 {
 
         return node;
     }
+
+    // Time: O(n), Space: O(n)
+    public TreeNode mergeTrees2(TreeNode t1, TreeNode t2) {
+        if (t1 == null && t2 == null) return null;
+        if (t1 != null && t2 == null) return t1;
+        if (t2 != null && t1 == null) return t2;
+        t1.val += t2.val;
+        t1.left = mergeTrees2(t1.left, t2.left);
+        t1.right = mergeTrees2(t1.right, t2.right);
+        return t1;
+    }
 }
