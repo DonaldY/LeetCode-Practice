9b454490f48f58b981d5bb1f3f143c5d468f8308
Donald <448641125@qq.com>
2019-09-06 10:25:43 +0800

    add bytedance second highest salary

---
+++ src/main/java/bytedance/other/SecondHighestSalary.sql
@@ -0,0 +1,28 @@
+/**
+拓展练习 - 第二高的薪水
+
+编写一个 SQL 查询，获取 Employee 表中第二高的薪水（Salary） 。
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+例如上述 Employee 表，SQL查询应该返回 200 作为第二高的薪水。如果不存在第二高的薪水，那么查询应返回 null。
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+
+Tips:
+1. 有记录(行数) 与 有行数(返回null) 的区别
+2. MAX() 聚合运算
+*/
+
+SELECT MAX(Salary) as SecondHighestSalary FROM Employee WHERE Salary < (SELECT MAX(Salary) FROM Employee);
+
+SELECT IFNULL((SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT 1, 1), NULL) as SecondHighestSalary;
