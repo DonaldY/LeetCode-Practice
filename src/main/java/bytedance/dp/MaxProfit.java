20dc586274d07f5e9dafb2ecb06697e555c4cfa7
Donald <448641125@qq.com>
2019-09-07 10:20:35 +0800

    add bytedance maxprofit

--- src/main/java/bytedance/dp/MaxProfit.java
+++ src/main/java/bytedance/dp/MaxProfit.java
@@ -1,4 +1,76 @@
 package bytedance.dp;
 
+/**
+ * 动态规划或贪心 - 买卖股票的最佳时机
+ *
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ *
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+ *
+ * 注意你不能在买入股票前卖出股票。
+ *
+ * 示例 1:
+ *
+ * 输入: [7,1,5,3,6,4]
+ * 输出: 5
+ * 解释: 在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+ *      注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格。
+ * 示例 2:
+ *
+ * 输入: [7,6,4,3,1]
+ * 输出: 0
+ * 解释: 在这种情况下, 没有交易完成, 所以最大利润为 0。
+ *
+ * 题意: 股票买卖最大收益
+ *
+ * 思路:
+ * 1. 两层遍历, 这个数往后的最大值
+ * 2. DP
+ */
 public class MaxProfit {
+
+    // Time : o(n ^ 2), Space: o(1)
+    public int maxProfit(int[] prices) {
+
+        if (prices == null || prices.length == 0) return 0;
+
+        int result = 0;
+
+        for (int i = 0; i < prices.length; ++i) {
+
+            for (int j = i + 1; j < prices.length; ++j) {
+
+                if (prices[i] > prices[j]) continue;
+
+                int num = prices[j] - prices[i];
+
+                if (result < num) result = num;
+            }
+        }
+
+        return result;
+    }
+
+    // Time: o(n), Space: o(1)
+    public int maxProfitWithDP(int [] prices) {
+
+        if (prices == null || prices.length == 0) return 0;
+
+        int min = prices[0];
+
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; ++i) {
+
+            if (min > prices[i]) {
+
+                min = prices[i];
+                continue;
+            }
+
+            maxProfit = Math.max(maxProfit, prices[i] - min);
+        }
+
+        return maxProfit;
+    }
 }
