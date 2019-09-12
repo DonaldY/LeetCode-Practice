45172891ed16807e77ba4e17dec34b227394d3e8
Donald <448641125@qq.com>
2019-09-12 13:18:03 +0800

    update lowest common ancestor

--- src/main/java/bytedance/linkedlist/LowestCommonAncestor.java
+++ src/main/java/bytedance/linkedlist/LowestCommonAncestor.java
@@ -102,4 +102,18 @@ public class LowestCommonAncestor {
 
         return null;
     }
+
+    public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+
+        if(root == null || root == p || root == q) return root;
+
+        TreeNode left = lowestCommonAncestor2(root.left, p, q);
+
+        TreeNode right = lowestCommonAncestor2(root.right, p, q);
+
+        if(left != null && right != null) return root;
+        if(left != null) return left;
+
+        return right;
+    }
 }
