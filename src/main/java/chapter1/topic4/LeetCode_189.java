d86544d1d61234e08c3d0a94daeeda61e01e4809
Donald <448641125@qq.com>
2019-09-23 09:39:32 +0800

    update leetcode 189

--- src/main/java/chapter1/topic4/LeetCode_189.java
+++ src/main/java/chapter1/topic4/LeetCode_189.java
@@ -29,7 +29,9 @@ import java.util.Stack;
  *
  * 思路：
  * 1. 辅助栈，用于保存旋转的数
- * 2.
+ * 2. 反转
+ *
+ * Tips：K 数可能较大
  */
 public class LeetCode_189 {
 
@@ -56,6 +58,38 @@ public class LeetCode_189 {
         while (!stack.isEmpty()) {
 
             nums[j ++] = stack.pop();
+        }
+    }
+
+    // Time: o(n), Space: o(n), Faster: 99.11%
+    public void rotateByCopy(int [] nums, int k) {
+
+        if (nums == null || nums.length == 0 || k <= 0) return;
+
+        int n = nums.length, m = k % n, i = 0;
+
+        int [] t = new int[n];
+
+        for (int j = n - m; j < n; ++j) t[i++] = nums[j];
+        for (int j = 0; j < n - m; ++j) t[i++] = nums[j];
+        for (int j = 0; j < n; ++j) nums[j] = t[j];
+    }
+
+    // Time: o(n), Space: o(1), Faster: 100.00%
+    public void rotateBySwap(int [] nums, int k) {
+        if (nums == null || nums.length == 0 || k <= 0) return;
+
+        int n = nums.length, m = k % n;
+        reverse(nums, 0, n - 1);
+        reverse(nums, 0, m - 1);
+        reverse(nums, m, n - 1);
+    }
+
+    private void reverse(int[] nums, int i, int j) {
+        for (; i < j; ++i, --j) {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
         }
     }
 }
