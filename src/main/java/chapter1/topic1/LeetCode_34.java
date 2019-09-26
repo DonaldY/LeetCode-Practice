e1268beeb5d6ce8ff39c33f61bf0289ea427932e
Donald <448641125@qq.com>
2019-09-26 23:04:23 +0800

    add leetcode 34

---
+++ src/main/java/chapter1/topic1/LeetCode_34.java
@@ -0,0 +1,77 @@
+package chapter1.topic1;
+
+/**
+ * 34. Find First and Last Position of Element in Sorted Array
+ *
+ * Given an array of integers nums sorted in ascending order, find the starting and ending position of a given target value.
+ *
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ *
+ * If the target is not found in the array, return [-1, -1].
+ *
+ * Example 1:
+ *
+ * Input: nums = [5,7,7,8,8,10], target = 8
+ * Output: [3,4]
+ * Example 2:
+ *
+ * Input: nums = [5,7,7,8,8,10], target = 6
+ * Output: [-1,-1]
+ *
+ * 题意： 在一个排序的数组中，找到给定数的开始位置和结束位置
+ *
+ * 思路：
+ * 1. 二分法
+ *    中中间开始找
+ *    1. 若找到了， 则向左边继续找
+ *    2. 若找到了， 则向右边继续找
+ */
+public class LeetCode_34 {
+
+    public static void main(String[] args) {
+
+        LeetCode_34 leetCode_34 = new LeetCode_34();
+
+        leetCode_34.searchRange(new int[]{5,7,7,8,8,10}, 8);
+    }
+
+    // Time: O(logn), Space: O(1), Faster: 100.00%
+    public int[] searchRange(int[] nums, int target) {
+
+        if (nums == null || nums.length == 0) return new int[]{-1, -1};
+
+        int start = -1, end = -1;
+
+        for (int left = 0, right = nums.length - 1; left <= right;) {
+
+            int mid = (right + left) / 2;
+            if (nums[mid] == target) {
+
+                start = mid;
+                break;
+            } else if (target < nums[mid]) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        while (start >= 1 && nums[start] == nums[start - 1]) {
+            --start;
+        }
+
+        for (int left = 0, right = nums.length - 1; left <= right;) {
+
+            int mid = (right + left) / 2;
+            if (nums[mid] == target) {
+
+                end = mid;
+                break;
+            } else if (target < nums[mid]) right = mid - 1;
+            else left = mid + 1;
+        }
+
+        while (end >= 0 && end <= nums.length - 2 && nums[end] == nums[end + 1]) {
+            ++end;
+        }
+
+        return new int[]{start, end};
+    }
+}
