967fe0574348a4567c55647a2cf9b2a58cddf125
Donald <448641125@qq.com>
2019-10-04 09:57:52 +0800

    update leetcode 210

--- src/main/java/chapter2/topic1/LeetCode_210.java
+++ src/main/java/chapter2/topic1/LeetCode_210.java
@@ -42,6 +42,7 @@ import java.util.Queue;
  *
  * 思路：
  * 1. 拓扑排序，出度和入度
+ * 2. DFS 查询
  */
 public class LeetCode_210 {
 
@@ -88,5 +89,40 @@ public class LeetCode_210 {
         if (count == numCourses) return result;
 
         return new int[0];
+    }
+
+    // Time: O(V+E), Space: O(V+E)
+    public int[] findOrderDFS(int n, int[][] pairs) {
+        List<List<Integer>> graph = new ArrayList<>(n);
+        for (int i = 0; i < n; ++i)
+            graph.add(new LinkedList<>());
+
+        for (int[] pair: pairs)
+            graph.get(pair[1]).add(pair[0]);
+
+        boolean[] checked = new boolean[n];
+        boolean[] visited = new boolean[n];
+        List<Integer> list = new ArrayList<>(n);
+        for (int i = 0; i < n; ++i)
+            if (!checked[i] && hasCycle(graph, visited, checked, list, i))
+                return new int[]{};
+
+        int[] order = new int[n];
+        for (int v: list)
+            order[--n] = v;
+        return order;
+    }
+
+    private boolean hasCycle(List<List<Integer>> graph, boolean[] visited,
+                             boolean[] checked, List<Integer> order, int v) {
+        if (visited[v]) return true;
+        visited[v] = true;
+        for (int i: graph.get(v))
+            if (!checked[i] && hasCycle(graph, visited, checked, order, i))
+                return true;
+        checked[v] = true;
+        order.add(v);
+        visited[v] = false;
+        return false;
     }
 }
