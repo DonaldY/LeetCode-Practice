59781509b230c9d4017ee30ce1c71e4b51edcf32
Donald <448641125@qq.com>
2019-10-16 11:38:44 +0800

    update leetcode 221

--- src/main/java/chapter2/topic1/LeetCode_221.java
+++ src/main/java/chapter2/topic1/LeetCode_221.java
@@ -21,16 +21,19 @@ package chapter2.topic1;
  *
  * 思路：
  * 1. 在 Leetcode 85上， 选择最小的然后相乘
+ *
+ * 2. 看似求解最长边长，然后用动态规划来求解 dp
+ *
  */
 public class LeetCode_221 {
 
     // Time: O(m * n), Space: O(n), Faster: 99.64%
     public int maximalSquare(char[][] matrix) {
 
-        return maximalRectangle(matrix);
+        return maximalSquare(matrix);
     }
 
-    private int maximalRectangle(char[][] matrix) {
+    private int maximalSquareInternal(char[][] matrix) {
         if (matrix == null || matrix.length == 0
                 || matrix[0] == null || matrix[0].length == 0)
             return 0;
@@ -41,12 +44,12 @@ public class LeetCode_221 {
             for (int j = 0; j < n ; ++j) {
                 heights[j] = matrix[i][j] == '1' ? heights[j] + 1 : 0;
             }
-            max = Math.max(max, largestRectangleHistogram(heights));
+            max = Math.max(max, largestSquareleHistogram(heights));
         }
         return max;
     }
 
-    private int largestRectangleHistogram(int [] heights) {
+    private int largestSquareleHistogram(int [] heights) {
         int max = 0, n = heights.length, top = -1;
         int [] st = new int[n + 1];
         for (int r = 0; r <= n; ++r) {
@@ -59,5 +62,29 @@ public class LeetCode_221 {
             st[++top] = r;
         }
         return max;
+    }
+
+    // Time: O(n * m), Space: O(n * m), Faster: 95.79%
+    public int maximalSquareDP(char[][] matrix) {
+        if (matrix == null || matrix.length == 0 ||
+         matrix[0] == null || matrix[0].length == 0)
+            return 0;
+        int m = matrix.length, n = matrix[0].length;
+        int [][] d = new int[m][n];
+        int len = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (i == 0 || j == 0 || matrix[i][j] == '0')
+                    d[i][j] = matrix[i][j] == '1'? 1 : 0;
+                else
+                    d[i][j] = min(d[i - 1][j], d[i][j - 1], d[i - 1][j - 1] + 1);
+                len = Math.max(len, d[i][j]);
+            }
+        }
+        return len * len;
+    }
+
+    private int min(int a, int b, int c) {
+        return Math.min(a, Math.min(b, c));
     }
 }
