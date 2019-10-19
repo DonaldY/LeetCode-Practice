26bae1f48941d24d1e7f44446a2f3e37c93194e3
Donald <448641125@qq.com>
2019-10-20 00:37:42 +0800

    add multi thread

--- src/main/java/multithreading/NumberI.java
+++ src/main/java/multithreading/NumberI.java
@@ -1,7 +1,59 @@
 package multithreading;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
- * 两个数组一样长，交替打印出来。(synchronized、wait、notify控制实现)
+ * 1~100，两个线程交替打印出来
+ *
+ * synchronized、wait、notify控制实现
  */
 public class NumberI {
+
+    static int num = 1;
+    static final Object lock = new Object();
+
+    public static void main(String[] args) {
+
+        Thread oddThread = new Thread(() -> {
+            synchronized (lock) {
+                while (num <= 100) {
+                    if (num % 2 == 1) {
+                        System.out.println(num++);
+                        lock.notify();
+
+                    } else {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }
+            }
+        });
+
+        Thread evenThread = new Thread(() -> {
+            synchronized (lock) {
+                while (num <= 100) {
+                    if (num % 2 == 0) {
+                        System.out.println(num++);
+                        lock.notify();
+                    } else {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                }
+            }
+        });
+
+        oddThread.start();
+        evenThread.start();
+    }
+
+
 }
