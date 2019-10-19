03b06f6af9b022b53b88d39c95f0a6f1483545c6
Donald <448641125@qq.com>
2019-10-19 19:56:26 +0800

    update leetcode 53

--- src/main/java/chapter1/topic2/LeetCode_53.java
+++ src/main/java/chapter1/topic2/LeetCode_53.java
@@ -10,6 +10,8 @@ package chapter1.topic2;
  * 思路：
  * 1. 暴力求法，两层for循环
  * 2. 遍历一遍，负数 + 负数没有任何意义
+ *    前面累加值为负数，则对后面没有正向的贡献， 所以可以直接舍弃前面这段子序列的和
+ *
  */
 public class LeetCode_53 {
 
@@ -17,7 +19,30 @@ public class LeetCode_53 {
 
         LeetCode_53 leetCode = new LeetCode_53();
 
+        // [-2,1,-3,4,-1,2,1,-5,4]
         System.out.println(leetCode.maxSumOfSubArray(new int[]{-2, 1, -3, 4, -1, 2, 1, -5, 4}));
+    }
+
+    // Time: O(n ^ 2), Space: O(1), Faster: 5.05%
+    public int maxSumOfSubArray2(int [] nums) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; ++i) {
+            int sum = nums[i];
+            max = Math.max(max, sum);
+            for(int j = i + 1; j < nums.length; ++j) {
+                sum += nums[j];
+                max = Math.max(max, sum);
+            }
+        }
+        return max;
+    }
+
+    public int test(int [] nums) {
+        int max = Integer.MIN_VALUE;
+        for (int cur = 0; cur < nums.length; ++cur) {
+
+        }
+        return 1;
     }
 
     // Time: O(n), Space: O(1), Faster: 95.05%
