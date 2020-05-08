864bdd5f7ee1a1bee21225f7a36231091a634e2f
Donald <448641125@qq.com>
2020-05-08 08:17:33 +0800

    README.md

---
+++ src/main/java/chapter1/topic3/LeetCode_124.java
@@ -0,0 +1,8 @@
+package chapter1.topic3;
+
+/**
+ * @author donald
+ * @date 5/8/20
+ */
+public class LeetCode_124 {
+}
--- src/main/java/chapter2/topic3/LeetCode_347.java
+++ src/main/java/chapter2/topic3/LeetCode_347.java
@@ -6,6 +6,8 @@ import java.util.*;
  * @author donald
  * @date 2020/5/7
  *
+ * 347. Top K Frequent Elements
+ *
  * Given a non-empty array of integers, return the k most frequent elements.
  *
  * Example 1:
@@ -28,7 +30,9 @@ import java.util.*;
  *
  * 思路：
  * 1. 暴力法，hash保存，最大堆
+ *
  * 2. 快速选择法
+ *
  */
 public class LeetCode_347 {
 
@@ -68,5 +72,44 @@ public class LeetCode_347 {
         }
 
         return result;
+    }
+
+    // Time(avg): O(n), Time(worst): O(n^2), Space: O(n), Faster: 86.69%
+    public int[] topKFrequentQuickSelect(int[] nums, int k) {
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int num: nums) {
+            int freq = freqMap.getOrDefault(num, 0);
+            freqMap.put(num, freq + 1);
+        }
+
+        List<Map.Entry<Integer, Integer>> entries = new ArrayList<>(freqMap.entrySet());
+        int low = 0, high = entries.size() - 1;
+        while (low <= high) {
+            int p = partition(entries, low, high);
+            if (p == k-1) break;
+            else if (p > k-1) high = p - 1;
+            else low = p + 1;
+        }
+
+        int[] result = new int[k];
+
+        for (int i = 0; i < k; ++i) {
+
+            result[i] = entries.get(i).getKey();
+        }
+
+        return result;
+    }
+
+    private int partition(List<Map.Entry<Integer, Integer>> entries, int low, int high) {
+        int pivot = entries.get(low).getValue();
+        int i = low, j = high;
+        while (i < j) {
+            while (i < j && entries.get(j).getValue() < pivot) --j;
+            if (i < j) Collections.swap(entries, i, j);
+            while (i < j && entries.get(i).getValue() >= pivot) ++i;
+            if (i < j) Collections.swap(entries, i, j);
+        }
+        return i;
     }
 }
