7ed079f817606689cac2323d972fea7172a8a457
Donald <448641125@qq.com>
2020-05-13 23:57:53 +0800

    add leetcode 16

---
+++ src/main/java/chapter1/topic1/LeetCode_16.java
@@ -0,0 +1,29 @@
+package chapter1.topic1;
+
+/**
+ * @author donald
+ * @date 20205/13
+ *
+ * 16. 3Sum Closest
+ *
+ * Given an array nums of n integers and an integer target, find three integers in nums such that the sum is closest to target.
+ * Return the sum of the three integers. You may assume that each input would have exactly one solution.
+ *
+ * Example:
+ *
+ * Given array nums = [-1, 2, 1, -4], and target = 1.
+ *
+ * The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+ *
+ * 题意：
+ * 从数组中找到3个数，这3个数相加的值需要离目标值最近
+ *
+ * 思路：
+ *
+ */
+public class LeetCode_16 {
+
+    public int threeSumClosest(int[] nums, int target) {
+
+    }
+}
