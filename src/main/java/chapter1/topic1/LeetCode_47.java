3d268294627db74aa54721aa4cd66ac93dd54f71
Donald <448641125@qq.com>
2020-05-27 11:02:04 +0800

    update leetcode 47

---
+++ src/main/java/chapter1/topic1/LeetCode_47.java
@@ -0,0 +1,62 @@
+package chapter1.topic1;
+
+import java.util.*;
+
+/**
+ * @author donald
+ * @date 2020/5/26
+ *
+ * 47. Permutations II
+ *
+ * Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+ *
+ * Example:
+ *
+ * Input: [1,1,2]
+ * Output:
+ * [
+ *   [1,1,2],
+ *   [1,2,1],
+ *   [2,1,1]
+ * ]
+ *
+ * 题意：
+ * 给一个可能有重复数字的数组，计算出其全排列
+ *
+ *
+ * 思路：
+ * 1. 在 leetcode_46 基础上，增加 hash，去掉重复数组
+ *
+ * 2.
+ *
+ */
+public class LeetCode_47 {
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+
+        if (null == nums || nums.length == 0) return Collections.emptyList();
+
+        Set<List<Integer>> result = new HashSet<>();
+
+        List<Integer> list = new ArrayList<>();
+        for (int num: nums) list.add(num);
+
+        permuteRec(list, 0, result);
+
+        return new ArrayList<>(result);
+    }
+
+    private void permuteRec(List<Integer> list, int start, Set<List<Integer>> result) {
+
+        if (start == list.size()) {
+            result.add(new ArrayList<>(list));
+            return;
+        }
+
+        for (int i = start; i < list.size(); ++i) {
+            Collections.swap(list, i , start);
+            permuteRec(list, start + 1, result);
+            Collections.swap(list, start, i);
+        }
+    }
+}
