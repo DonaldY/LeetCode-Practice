cd3ad8ee010d26da678fb3f20deeb5b62d3e80c4
Donald <448641125@qq.com>
2020-11-22 20:46:43 +0800

    update leetcode 206

--- src/main/java/chapter2/topic1/LeetCode_206.java
+++ src/main/java/chapter2/topic1/LeetCode_206.java
@@ -20,37 +20,6 @@ class ListNode {
 
 public class LeetCode_206 {
 
-    public static void main(String[] args) {
-
-        ListNode node1 = initNode();
-
-        LeetCode_206 leetCode = new LeetCode_206();
-        ListNode node = leetCode.reverseList(node1);
-
-        while (node != null) {
-
-            System.out.println(node.val);
-            node = node.next;
-        }
-    }
-
-    private static ListNode initNode() {
-
-        ListNode node1 = new ListNode(1);
-        ListNode node2 = new ListNode(2);
-        ListNode node3 = new ListNode(3);
-        ListNode node4 = new ListNode(4);
-        ListNode node5 = new ListNode(5);
-
-        node1.next = node2;
-        node2.next = node3;
-        node3.next = node4;
-        node4.next = node5;
-        node5.next = null;
-
-        return node1;
-    }
-
     // Time: o(n) Space: o(1) faster: 100%
     public ListNode reverseList(ListNode head) {
 
