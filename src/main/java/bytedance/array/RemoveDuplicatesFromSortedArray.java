3e17d41c850d9047085c2da1c6ab4a863bef6354
Donald <448641125@qq.com>
2020-11-24 10:47:49 +0800

    update leetcode 26

--- src/main/java/bytedance/array/RemoveDuplicatesFromSortedArray.java
+++ src/main/java/bytedance/array/RemoveDuplicatesFromSortedArray.java
@@ -22,6 +22,10 @@ package bytedance.array;
  *          ^^
  *          ||
  *          sf
+ *
+ * 2. 数组完成排序后，我们可以放置两个指针 i 和 j，其中 ii 是慢指针，而 j 是快指针。只要 nums[i] = nums[j]，我们就增加 j 以跳过重复项。
+ * 当我们遇到 nums[j] != nums[j] 时，跳过重复项的运行已经结束，
+ * 因此我们必须把它（nums[j]）的值复制到 nums[i+1]。然后递增 i，接着我们将再次重复相同的过程，直到 j 到达数组的末尾为止。
  */
 public class RemoveDuplicatesFromSortedArray {
 
@@ -36,7 +40,7 @@ public class RemoveDuplicatesFromSortedArray {
         for (int i = 0; i < nums.length; ++i) System.out.println(nums[i]);
     }
 
-    // Time: O(n), Space: O(1), Faster: 100.00%
+    // 方法 1 ： Time: O(n), Space: O(1), Faster: 100.00%
     public int removeDuplicates(int[] nums) {
 
         if (nums == null || nums.length == 0) return 0;
@@ -53,5 +57,18 @@ public class RemoveDuplicatesFromSortedArray {
         }
 
         return slow;
+    }
+
+    // 方法 2 ： Time: O(n), Space: O(1), Faster: 100.00%
+    public int removeDuplicatesOffice(int[] nums) {
+        if (nums.length == 0) return 0;
+        int i = 0;
+        for (int j = 1; j < nums.length; j++) {
+            if (nums[j] != nums[i]) {
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i + 1;
     }
 }
