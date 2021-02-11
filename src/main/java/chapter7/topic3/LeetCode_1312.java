89f6544de25b17c1a3477f6854057c5f3eb153cb
Donald <448641125@qq.com>
2021-02-11 22:03:11 +0800

    update leetcode 1312

--- src/main/java/chapter7/topic3/LeetCode_1312.java
+++ src/main/java/chapter7/topic3/LeetCode_1312.java
@@ -72,4 +72,31 @@ public class LeetCode_1312 {
         // 根据 dp 数组的定义，题目要求的答案是 dp[0][n - 1]
         return dp[0][n - 1];
     }
+
+    // Time: O(n ^ 2), Space: O(n), Faster: 77.81%
+    public int minInsertions2(String s) {
+        int n = s.length();
+
+        int [] dp = new int[n];
+
+        int temp = 0;
+        for (int i = n - 2; i >= 0; --i) {
+            // 记录 dp[i+1][j-1]
+            int pre = 0;
+            for (int j = i + 1; j < n; ++j) {
+                temp = dp[j];
+
+                if (s.charAt(i) == s.charAt(j)) {
+                    // dp[i][j] = dp[i+1][j-1];
+                    dp[j] = pre;
+                } else {
+                    // dp[i][j] = Math.min(dp[i + 1][j], dp[i][j - 1]) + 1;
+                    dp[j] = Math.min(dp[j], dp[j - 1]) + 1;
+                }
+                pre = temp;
+            }
+        }
+
+        return dp[n - 1];
+    }
 }
