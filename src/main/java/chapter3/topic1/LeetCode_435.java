4cace43d74a400e9dc0f3ecb50772f889af85bf7
Donald <448641125@qq.com>
2021-02-18 00:19:31 +0800

    add leetcode 435

---
+++ src/main/java/chapter3/topic1/LeetCode_435.java
@@ -0,0 +1,81 @@
+package chapter3.topic1;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * @author donald
+ * @date 2021/02/18
+ *
+ * 435. 无重叠区间
+ * 给定一个区间的集合，找到需要移除区间的最小数量，使剩余区间互不重叠。
+ *
+ * 注意:
+ *
+ * 可以认为区间的终点总是大于它的起点。
+ * 区间 [1,2] 和 [2,3] 的边界相互“接触”，但没有相互重叠。
+ *
+ * ```
+ * 示例 1:
+ *
+ * 输入: [ [1,2], [2,3], [3,4], [1,3] ]
+ *
+ * 输出: 1
+ *
+ * 解释: 移除 [1,3] 后，剩下的区间没有重叠。
+ * 示例 2:
+ *
+ * 输入: [ [1,2], [1,2], [1,2] ]
+ *
+ * 输出: 2
+ *
+ * 解释: 你需要移除两个 [1,2] 来使剩下的区间没有重叠。
+ * 示例 3:
+ *
+ * 输入: [ [1,2], [2,3] ]
+ *
+ * 输出: 0
+ *
+ * 解释: 你不需要移除任何区间，因为它们已经是无重叠的了。
+ * ```
+ *
+ * 题意： 至少去掉多少个重叠区域，才能有无重叠区域
+ *
+ * 思路：
+ * 反向思考，最多有多少不重叠区域，总数减去即可。
+ */
+public class LeetCode_435 {
+
+    // Time: O(NlogN), Space: O(1), Faster: 80.57%
+    public int eraseOverlapIntervals(int[][] intervals) {
+
+        int n = intervals.length;
+
+        return n - intervals(intervals);
+    }
+
+    private int intervals(int[][] intervals) {
+
+        if (intervals.length == 0) return 0;
+        // 按照 end 升序排序
+        Arrays.sort(intervals, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+        // 至少有一个区间不相交
+        int count = 1;
+        // 排序后，第一个区间就是 x
+        int xEnd = intervals[0][1];
+        for (int [] ints : intervals) {
+            int start = ints[0];
+            if (start >= xEnd) {
+                // 找到下一个选择的区间了
+                ++count;
+                xEnd = ints[1];
+            }
+        }
+        return count;
+    }
+}
