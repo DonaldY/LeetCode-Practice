e835b0e583b40f24adfa178a8c50ec12414ba4ec
Donald <448641125@qq.com>
2021-03-06 17:52:26 +0800

    update leetcode 503

--- src/main/java/chapter3/topic3/LeetCode_503.java
+++ src/main/java/chapter3/topic3/LeetCode_503.java
@@ -1,5 +1,8 @@
 package chapter3.topic3;
 
+import java.util.Arrays;
+import java.util.Stack;
+
 /**
  * @author donald
  * @date 2021/03/06
@@ -27,6 +30,12 @@ package chapter3.topic3;
  *
  * 思路：
  * 1. 暴力解法，模拟循环
+ * 2. 单调递减栈。
+ *    1. 建立「单调递减栈」，并对原数组遍历一次：
+ *       - 如果栈为空，则把当前元素放入栈内；
+ *       - 如果栈不为空，则需要判断当前元素和栈顶元素的大小：
+ *       - 如果当前元素比栈顶元素大：说明当前元素是前面一些元素的「下一个更大元素」，则逐个弹出栈顶元素，直到当前元素比栈顶元素小为止。
+ *       - 如果当前元素比栈顶元素小：说明当前元素的「下一个更大元素」与栈顶元素相同，则把当前元素入栈。
  */
 public class LeetCode_503 {
 
@@ -47,6 +56,24 @@ public class LeetCode_503 {
             }
             result[i] = nextMax == nums[i] ? -1 : nextMax;
         }
+        return result;
+    }
+
+    // 2. 单调递减栈
+    // Time: O(n), Space: O(n), Faster: 19.51%
+    public int[] nextGreaterElementsWithStack(int[] nums) {
+
+        if (null == nums || nums.length == 0) return new int[0];
+        int[] result = new int[nums.length];
+        Arrays.fill(result, -1);
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < 2 * nums.length; i++) {
+            while (!stack.isEmpty() && nums[i % nums.length] > nums[stack.peek()]) {
+                result[stack.pop()] = nums[i % nums.length];
+            }
+            stack.push(i % nums.length);
+        }
+
         return result;
     }
 }
