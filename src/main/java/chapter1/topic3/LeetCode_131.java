b3fc01775133621703365d343c89843fb689c3c0
Donald <448641125@qq.com>
2021-03-08 08:30:18 +0800

    update leetcode 131

--- src/main/java/chapter1/topic3/LeetCode_131.java
+++ src/main/java/chapter1/topic3/LeetCode_131.java
@@ -29,37 +29,23 @@ import java.util.List;
  */
 public class LeetCode_131 {
 
-    // Time: o(2 ^ n), Space: o(n ^ 2), Faster: 97.45%
-    public List<List<String>> partition(String s) {
-
-        if (s == null || s.length() == 0) return Collections.emptyList();
-        List<List<String>> result = new ArrayList<>();
-        int n = s.length();
+    // Time: O(n^2), Space: O(n^2)
+    public int countPalindromicSubstringsDP(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int n = s.length(), count = 0;
         boolean[][] d = new boolean[n][n];
+
         for (int i = n - 1; i >= 0; --i) {
             for (int j = i; j < n; ++j) {
                 if (i == j) d[i][j] = true;
-                else if (i + 1 == j) d[i][j] = s.charAt(i) == s.charAt(j);
-                else d[i][j] = s.charAt(i) == s.charAt(j) && d[i + 1][j - 1];
-            }
-        }
-        partition(s, 0, d, result, new ArrayList<>());
-        return result;
-    }
+                else if (i+1 == j) d[i][j] = s.charAt(i) == s.charAt(j);
+                else d[i][j] = s.charAt(i) == s.charAt(j) && d[i+1][j-1];
 
-    private void partition(String s, int start, boolean[][] d,
-                           List<List<String>> result, List<String> elem) {
-        if (start >= s.length()) {
-            result.add(new ArrayList<>(elem));
-        } else {
-            for (int end = start; end < s.length(); ++end) {
-                if (d[start][end]) {
-                    elem.add(s.substring(start, end + 1));
-                    partition(s, end + 1, d, result, elem);
-                    elem.remove(elem.size() - 1);
-                }
+                if (d[i][j]) ++count;
             }
         }
+
+        return count;
     }
 
     // Time: O(n^2), Space: O(1)
