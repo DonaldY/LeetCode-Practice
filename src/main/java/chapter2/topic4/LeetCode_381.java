b34d467971676ec8c38f6b9ee0702e6fb885a59c
Donald <448641125@qq.com>
2021-03-17 11:02:00 +0800

    update leetcode 380 381

--- src/main/java/chapter2/topic4/LeetCode_381.java
+++ src/main/java/chapter2/topic4/LeetCode_381.java
@@ -13,9 +13,15 @@ import java.util.*;
  *
  * 注意，这 3 个操作的平均时间复杂度都要求是 O(1)。另外，这个集合中允许存储重复元素，一个元素被随机返回的概率与它在集合中的数量正相关。
  *
- * 难点在于： 解决冲突，一般使用拉链法
+ * 难点在于：
+ * 1. 操作时间复杂度为 0(1)
+ * 2. 每个数随机
+ * 3. 解决冲突，一般使用拉链法
  *
  * 思路：
+ * 方法一：
+ *    1. HashMap 存储值， key 为 值， value 为 HashSet 解决冲突， value 存储下标
+ *    2. 随机： 将所有的数均放到一个 list 中，取的时候随机 random
  */
 public class LeetCode_381 {
 
@@ -26,6 +32,7 @@ public class LeetCode_381 {
         private final List<Integer> data = new ArrayList<>();
         private final Random RANDOM = new Random();
 
+        // 新增操作
         public boolean insert(int val) {
             boolean existed = map.containsKey(val);
             if (!existed) map.put(val, new HashSet<>());
@@ -34,11 +41,14 @@ public class LeetCode_381 {
             return !existed;
         }
 
+        // 移除操作
         public boolean remove(int val) {
             if (!map.containsKey(val)) return false;
             int idx = map.get(val).iterator().next();
             map.get(val).remove(idx);
             int lastIdx = data.size() - 1;
+            // 解决删除后，数组移动，所有均删除最后一个
+            // 最后一个不是要删除的值，则先交换位置
             if (idx != lastIdx) {
                 int lastVal = data.get(lastIdx);
                 data.set(idx, lastVal);
@@ -50,6 +60,7 @@ public class LeetCode_381 {
             return true;
         }
 
+        // 获取随机数
         public int getRandom() {
             int idx = RANDOM.nextInt(data.size());
             return data.get(idx);
