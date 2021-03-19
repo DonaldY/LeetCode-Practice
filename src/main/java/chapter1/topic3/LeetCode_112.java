255d4333ddb19b22c5b069c2ec6465111a81133a
Donald <448641125@qq.com>
2021-03-19 08:31:27 +0800

    update leetcode 112

--- src/main/java/chapter1/topic3/LeetCode_112.java
+++ src/main/java/chapter1/topic3/LeetCode_112.java
@@ -20,7 +20,10 @@ import java.util.Stack;
  * 题意： 给一课二叉树和一个数，判断是否存在从根节点到叶子所有值之和等于一个数
  *
  * 思路：
- * 1. dfs一个个路径判断
+ * 1. dfs一个个路径判断:
+ *    递归法，每递归到一个节点，就拿target-当前节点值，如果存在路径总和等于target，那么最后一个叶子节点一定等于target剩下的值。
+ *    如果递归到叶子节点后，剩余的target值不等于叶子节点，说明不存在路径总和等于target。
+ *
  * 2. 迭代方式： 使用栈
  */
 public class LeetCode_112 {
@@ -30,6 +33,7 @@ public class LeetCode_112 {
 
         if (root == null) return false;
 
+        // 叶子节点
         if (root.left == null && root.right == null) {
 
             return root.val == sum;
