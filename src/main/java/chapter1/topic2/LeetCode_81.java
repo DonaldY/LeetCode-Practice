200b3905400d770e81f6dcb0708bce1b71deb4e2
Donald <448641125@qq.com>
2021-04-07 23:57:50 +0800

    add leetcode 81

---
+++ src/main/java/chapter1/topic2/LeetCode_81.java
@@ -0,0 +1,42 @@
+package chapter1.topic2;
+
+/**
+ * @author donald
+ * @date 2021/04/07
+ */
+public class LeetCode_81 {
+
+    public boolean search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return false;
+        }
+        if (n == 1) {
+            return nums[0] == target;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[l] == nums[mid] && nums[mid] == nums[r]) {
+                ++l;
+                --r;
+            } else if (nums[l] <= nums[mid]) {
+                if (nums[l] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
