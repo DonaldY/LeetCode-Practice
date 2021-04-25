ecaa49be407aaf7555f9eea79c75bb38b091ee17
Donald <448641125@qq.com>
2021-04-26 00:01:16 +0800

    add leetcode 897

--- src/main/java/chapter5/topic2/LeetCode_897.java
+++ src/main/java/chapter5/topic2/LeetCode_897.java
@@ -1,15 +1,39 @@
 package chapter5.topic2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author donald
  * @date 2021/04/25
- *
+ * <p>
  * LeetCode 897. 递增顺序搜索树
+ * <p>
+ * 给你一棵二叉搜索树，请你 按中序遍历
+ * 将其重新排列为一棵递增顺序搜索树，使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右子节点。
  */
 public class LeetCode_897 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
 
     public TreeNode increasingBST(TreeNode root) {
-        List<Integer> res = new ArrayList<Integer>();
+        List<Integer> res = new ArrayList<>();
         inorder(root, res);
 
         TreeNode dummyNode = new TreeNode(-1);
