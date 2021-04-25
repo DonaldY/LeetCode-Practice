c54002a3b3f0aadcda9502249cf760d958285336
Donald <448641125@qq.com>
2021-04-25 23:59:41 +0800

    add leetcode 897

---
+++ src/main/java/chapter5/topic2/LeetCode_897.java
@@ -0,0 +1,32 @@
+package chapter5.topic2;
+
+/**
+ * @author donald
+ * @date 2021/04/25
+ *
+ * LeetCode 897. 递增顺序搜索树
+ */
+public class LeetCode_897 {
+
+    public TreeNode increasingBST(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        inorder(root, res);
+
+        TreeNode dummyNode = new TreeNode(-1);
+        TreeNode currNode = dummyNode;
+        for (int value : res) {
+            currNode.right = new TreeNode(value);
+            currNode = currNode.right;
+        }
+        return dummyNode.right;
+    }
+
+    public void inorder(TreeNode node, List<Integer> res) {
+        if (node == null) {
+            return;
+        }
+        inorder(node.left, res);
+        res.add(node.val);
+        inorder(node.right, res);
+    }
+}
