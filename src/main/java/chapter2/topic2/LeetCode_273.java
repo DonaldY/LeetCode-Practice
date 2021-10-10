6c5341318a52aff77b9b7d87afc578f8be40b331
Donald <448641125@qq.com>
2021-10-11 01:15:06 +0800

    add leetcode 273

---
+++ src/main/java/chapter2/topic2/LeetCode_273.java
@@ -0,0 +1,72 @@
+package chapter2.topic2;
+
+/**
+ * 273. Integer to English Words
+ *
+ * Convert a non-negative integer num to its English words representation.
+ *
+ *  
+ *
+ * Example 1:
+ *
+ * Input: num = 123
+ * Output: "One Hundred Twenty Three"
+ * Example 2:
+ *
+ * Input: num = 12345
+ * Output: "Twelve Thousand Three Hundred Forty Five"
+ * Example 3:
+ *
+ * Input: num = 1234567
+ * Output: "One Million Two Hundred Thirty Four Thousand Five Hundred Sixty Seven"
+ * Example 4:
+ *
+ * Input: num = 1234567891
+ * Output: "One Billion Two Hundred Thirty Four Million Five Hundred Sixty Seven Thousand Eight Hundred Ninety One"
+ *
+ *
+ */
+public class LeetCode_273 {
+
+    String[] singles = {"", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+    String[] teens = {"Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen",
+            "Eighteen", "Nineteen"};
+    String[] tens = {"", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+    String[] thousands = {"", "Thousand", "Million", "Billion"};
+
+    // Time: O(1), Space: O(1), Faster: 92.33%
+    public String numberToWords(int num) {
+        if (num == 0) {
+            return "Zero";
+        }
+        StringBuffer sb = new StringBuffer();
+        for (int i = 3, unit = 1000000000; i >= 0; i--, unit /= 1000) {
+            int curNum = num / unit;
+            if (curNum != 0) {
+                num -= curNum * unit;
+                sb.append(toEnglish(curNum)).append(thousands[i]).append(" ");
+            }
+        }
+        return sb.toString().trim();
+    }
+
+    public String toEnglish(int num) {
+        StringBuffer curr = new StringBuffer();
+        int hundred = num / 100;
+        num %= 100;
+        if (hundred != 0) {
+            curr.append(singles[hundred]).append(" Hundred ");
+        }
+        int ten = num / 10;
+        if (ten >= 2) {
+            curr.append(tens[ten]).append(" ");
+            num %= 10;
+        }
+        if (num > 0 && num < 10) {
+            curr.append(singles[num]).append(" ");
+        } else if (num >= 10) {
+            curr.append(teens[num - 10]).append(" ");
+        }
+        return curr.toString();
+    }
+}
