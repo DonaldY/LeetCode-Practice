2e8bc8c2241949b1c98fd674ede2d71051c0fbfc
Donald <448641125@qq.com>
2021-10-12 02:41:04 +0800

    add leetcode 29

---
+++ src/main/java/chapter1/topic1/LeetCode_29.java
@@ -0,0 +1,114 @@
+package chapter1.topic1;
+
+/**
+ * @author donald
+ * @date 2021/10/12
+ */
+
+/**
+ * 29. 两数相除
+ *
+ * 给定两个整数，被除数 dividend 和除数 divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+ *
+ * 返回被除数 dividend 除以除数 divisor 得到的商。
+ *
+ * 整数除法的结果应当截去（truncate）其小数部分，例如：truncate(8.345) = 8 以及 truncate(-2.7335) = -2
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ * 输入: dividend = 10, divisor = 3
+ * 输出: 3
+ * 解释: 10/3 = truncate(3.33333..) = truncate(3) = 3
+ * 示例 2:
+ *
+ * 输入: dividend = 7, divisor = -3
+ * 输出: -2
+ * 解释: 7/-3 = truncate(-2.33333..) = -2
+ *
+ *
+ *
+ * 思路：
+ * 1. 位运算：模拟除
+ * 2. 二分查找 + 快速称
+ */
+public class LeetCode_29 {
+
+    public int divide(int dividend, int divisor) {
+        // 考虑被除数为最小值的情况
+        if (dividend == Integer.MIN_VALUE) {
+            if (divisor == 1) {
+                return Integer.MIN_VALUE;
+            }
+            if (divisor == -1) {
+                return Integer.MAX_VALUE;
+            }
+        }
+        // 考虑除数为最小值的情况
+        if (divisor == Integer.MIN_VALUE) {
+            return dividend == Integer.MIN_VALUE ? 1 : 0;
+        }
+        // 考虑被除数为 0 的情况
+        if (dividend == 0) {
+            return 0;
+        }
+
+        // 一般情况，使用二分查找
+        // 将所有的正数取相反数，这样就只需要考虑一种情况
+        boolean rev = false;
+        if (dividend > 0) {
+            dividend = -dividend;
+            rev = !rev;
+        }
+        if (divisor > 0) {
+            divisor = -divisor;
+            rev = !rev;
+        }
+
+        int left = 1, right = Integer.MAX_VALUE, ans = 0;
+        while (left <= right) {
+            // 注意溢出，并且不能使用除法
+            int mid = left + ((right - left) >> 1);
+            boolean check = quickAdd(divisor, mid, dividend);
+            if (check) {
+                ans = mid;
+                // 注意溢出
+                if (mid == Integer.MAX_VALUE) {
+                    break;
+                }
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return rev ? -ans : ans;
+    }
+
+    // 快速乘
+    public boolean quickAdd(int y, int z, int x) {
+        // x 和 y 是负数，z 是正数
+        // 需要判断 z * y >= x 是否成立
+        int result = 0, add = y;
+        while (z != 0) {
+            if ((z & 1) != 0) {
+                // 需要保证 result + add >= x
+                if (result < x - add) {
+                    return false;
+                }
+                result += add;
+            }
+            if (z != 1) {
+                // 需要保证 add + add >= x
+                if (add < x - add) {
+                    return false;
+                }
+                add += add;
+            }
+            // 不能使用除法
+            z >>= 1;
+        }
+        return true;
+    }
+}
