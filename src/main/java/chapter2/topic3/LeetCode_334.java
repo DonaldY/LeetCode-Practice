8ce95e1770a12a9122abd492ac3b85cbc5704a7c
Donald <448641125@qq.com>
2022-01-13 00:52:25 +0800

    update leetcode 334

--- src/main/java/chapter2/topic3/LeetCode_334.java
+++ src/main/java/chapter2/topic3/LeetCode_334.java
@@ -3,11 +3,69 @@ package chapter2.topic3;
 /**
  * @author donald
  * @date 2022/01/12
+ *
+ * 334. 递增的三元子序列
+ *
+ * 给你一个整数数组 nums ，判断这个数组中是否存在长度为 3 的递增子序列。
+ *
+ * 如果存在这样的三元组下标 (i, j, k) 且满足 i < j < k ，使得 nums[i] < nums[j] < nums[k] ，返回 true ；否则，返回 false 。
+ *
+ * 示例 1：
+ * 输入：nums = [1,2,3,4,5]
+ * 输出：true
+ * 解释：任何 i < j < k 的三元组都满足题意
+ *
+ *
+ * 示例 2：
+ * 输入：nums = [5,4,3,2,1]
+ * 输出：false
+ * 解释：不存在满足题意的三元组
+ *
+ *
+ * 示例 3：
+ * 输入：nums = [2,1,5,0,4,6]
+ * 输出：true
+ * 解释：三元组 (3, 4, 5) 满足题意，因为 nums[3] == 0 < nums[4] == 4 < nums[5] == 6
+ *
+ * 题意： 找到三元数
+ *
+ * 思路：
+ * 1. 暴力法： 遍历3次，分别找到最小、中间、最大的数
+ * 2. 贪心法：
+ *    如果 num > second，则找到了一个递增的三元子序列，返回 true；
+ *    否则，如果 num > first，则将 second 的值更新为 num；
+ *    否则，将 first 的值更新为 num。
+ *
  */
 public class LeetCode_334 {
 
+    // 暴力法
+    // Time: O(n), Space: O(1), Faster:
+    public boolean increasingTriplet0(int[] nums) {
+        int n = nums.length;
+        if (n < 3) {
+            return false;
+        }
+        int[] leftMin = new int[n];
+        leftMin[0] = nums[0];
+        for (int i = 1; i < n; i++) {
+            leftMin[i] = Math.min(leftMin[i - 1], nums[i]);
+        }
+        int[] rightMax = new int[n];
+        rightMax[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            rightMax[i] = Math.max(rightMax[i + 1], nums[i]);
+        }
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i] > leftMin[i - 1] && nums[i] < rightMax[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     // 贪心算法
-    // Faster: 78%
+    // Time: O(n), Space: O(1), Faster: 78%
     public boolean increasingTriplet(int[] nums) {
         int n = nums.length;
         if (n < 3) {
