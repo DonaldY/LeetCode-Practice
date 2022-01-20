9dbae2f49014967f201c9800aa3ca97c664e4f02
Donald <448641125@qq.com>
2022-01-20 09:20:14 +0800

    add leetcode 219

---
+++ src/main/java/chapter2/topic1/LeetCode_219.java
@@ -0,0 +1,69 @@
+package chapter2.topic1;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * 219. 存在重复元素 II
+ *
+ * @author donald
+ * @date 2022/01/19
+ *
+ * 给你一个整数数组 nums 和一个整数 k ，判断数组中是否存在两个 不同的索引 i 和 j ，
+ * 满足 nums[i] == nums[j] 且 abs(i - j) <= k 。如果存在，返回 true ；否则，返回 false 。
+ *
+ * 示例 1：
+ * 输入：nums = [1,2,3,1], k = 3
+ * 输出：true
+ *
+ * 示例 2：
+ * 输入：nums = [1,0,1,1], k = 1
+ * 输出：true
+ *
+ * 示例 3：
+ * 输入：nums = [1,2,3,1,2,3], k = 2
+ * 输出：false
+ *
+ * 题意： 比距离
+ *
+ * 思路：
+ * 1. 哈希表
+ * 2. 活动窗口
+ */
+public class LeetCode_219 {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int length = nums.length;
+
+        for (int i = 0; i < length; i++) {
+            int num = nums[i];
+
+            if (map.containsKey(num) && i - map.get(num) <= k) {
+                return true;
+            }
+            map.put(num, i);
+        }
+        return false;
+    }
+
+    // Faster: 94.49%
+    public boolean containsNearbyDuplicate2(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+}
