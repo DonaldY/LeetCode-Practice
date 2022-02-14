61e8d138d54b4ef535acce4e5741f510db59d51e
Donald <448641125@qq.com>
2022-02-15 00:01:26 +0800

    Update: add leetcode 1380

---
+++ src/main/java/chapter7/topic4/LeetCode_1380.java
@@ -0,0 +1,98 @@
+package chapter7.topic4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author donald
+ * @date 2022/02/15
+ *
+ * 1380. 矩阵中的幸运数
+ *
+ * 给你一个 m * n 的矩阵，矩阵中的数字 各不相同 。请你按 任意 顺序返回矩阵中的所有幸运数。
+ *
+ * 幸运数是指矩阵中满足同时下列两个条件的元素：
+ *
+ * 在同一行的所有元素中最小
+ * 在同一列的所有元素中最大
+ *  
+ *
+ * 示例 1：
+ * 输入：matrix = [[3,7,8],[9,11,13],[15,16,17]]
+ * 输出：[15]
+ * 解释：15 是唯一的幸运数，因为它是其所在行中的最小值，也是所在列中的最大值。
+ *
+ * 示例 2：
+ * 输入：matrix = [[1,10,4,2],[9,3,8,7],[15,16,17,12]]
+ * 输出：[12]
+ * 解释：12 是唯一的幸运数，因为它是其所在行中的最小值，也是所在列中的最大值。
+ *
+ * 示例 3：
+ * 输入：matrix = [[7,8],[1,2]]
+ * 输出：[7]
+ *
+ * 题意： 找最大值
+ *
+ * 思路：
+ * 1. 暴力法：遍历
+ */
+public class LeetCode_1380 {
+
+    public List<Integer> luckyNumbers (int[][] matrix) {
+
+        if (null == matrix || matrix.length == 0) return Collections.emptyList();
+
+        int m = matrix.length, n = matrix[0].length;
+        List<Integer> result = new ArrayList<>(matrix.length);
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                boolean isMin = true, isMax = true;
+                for (int k = 0; k < n; k++) {
+                    if (matrix[i][k] < matrix[i][j]) {
+                        isMin = false;
+                        break;
+                    }
+                }
+                if (!isMin) {
+                    continue;
+                }
+                for (int k = 0; k < m; k++) {
+                    if (matrix[k][j] > matrix[i][j]) {
+                        isMax = false;
+                        break;
+                    }
+                }
+                if (isMax) {
+                    result.add(matrix[i][j]);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public List<Integer> luckyNumbers2 (int[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int[] minRow = new int[m];
+        Arrays.fill(minRow, Integer.MAX_VALUE);
+        int[] maxCol = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                minRow[i] = Math.min(minRow[i], matrix[i][j]);
+                maxCol[j] = Math.max(maxCol[j], matrix[i][j]);
+            }
+        }
+        List<Integer> ret = new ArrayList<Integer>();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (matrix[i][j] == minRow[i] && matrix[i][j] == maxCol[j]) {
+                    ret.add(matrix[i][j]);
+                }
+            }
+        }
+        return ret;
+    }
+}
