e5e5febe1bb8b03e4dc550476a04e19a20aa02e2
Donald <448641125@qq.com>
2022-02-25 23:41:47 +0800

    Update: add leetcode 537

---
+++ src/main/java/chapter3/topic3/LeetCode_537.java
@@ -0,0 +1,43 @@
+package chapter3.topic3;
+
+/**
+ * @author donald
+ * @date 2022/02/25
+ *
+ * 537. 复数乘法
+ *
+ * 复数 可以用字符串表示，遵循 "实部+虚部i" 的形式，并满足下述条件：
+ *
+ * 实部 是一个整数，取值范围是 [-100, 100]
+ * 虚部 也是一个整数，取值范围是 [-100, 100]
+ * i2 == -1
+ * 给你两个字符串表示的复数 num1 和 num2 ，请你遵循复数表示形式，返回表示它们乘积的字符串。
+ *
+ *
+ * 示例 1：
+ * 输入：num1 = "1+1i", num2 = "1+1i"
+ * 输出："0+2i"
+ * 解释：(1 + i) * (1 + i) = 1 + i2 + 2 * i = 2i ，你需要将它转换为 0+2i 的形式。
+ *
+ *
+ * 示例 2：
+ * 输入：num1 = "1+-1i", num2 = "1+-1i"
+ * 输出："0+-2i"
+ * 解释：(1 - i) * (1 - i) = 1 + i2 - 2 * i = -2i ，你需要将它转换为 0+-2i 的形式。
+ *
+ * 题意： 复数相乘
+ *
+ * 思路： 模拟
+ */
+public class LeetCode_537 {
+
+    public String complexNumberMultiply(String num1, String num2) {
+        String[] complex1 = num1.split("\\+|i");
+        String[] complex2 = num2.split("\\+|i");
+        int real1 = Integer.parseInt(complex1[0]);
+        int imag1 = Integer.parseInt(complex1[1]);
+        int real2 = Integer.parseInt(complex2[0]);
+        int imag2 = Integer.parseInt(complex2[1]);
+        return String.format("%d+%di", real1 * real2 - imag1 * imag2, real1 * imag2 + imag1 * real2);
+    }
+}
