8fd16fef2959ec2b9d2456dd4654a1f6dde0e7f4
Donald <448641125@qq.com>
2022-03-01 23:39:40 +0800

    Update: update leetcode 6

--- src/main/java/chapter1/topic1/LeetCode_6.java
+++ src/main/java/chapter1/topic1/LeetCode_6.java
@@ -6,6 +6,18 @@ import java.util.List;
 /**
  * LeetCode 6 : ZigZag Conversion
  *
+ * 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+ *
+ * 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+ *
+ * P   A   H   N
+ * A P L S I I G
+ * Y   I   R
+ * 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+ *
+ * 请你实现这个将字符串进行指定行数变换的函数：
+ *
+ * string convert(string s, int numRows);
  *
  */
 public class LeetCode_6 {
@@ -20,6 +32,7 @@ public class LeetCode_6 {
         // PINALSIGYAHRPI
     }
 
+    // Faster: 38.97%
     public String convert(String s, int numRows) {
 
         char [] arr = s.toCharArray();
@@ -28,7 +41,7 @@ public class LeetCode_6 {
 
         for (int i = 0; i < numRows; ++i) {
 
-            outputLists.add(new ArrayList<Character>());
+            outputLists.add(new ArrayList<>());
         }
 
         int index = 0;
@@ -69,5 +82,20 @@ public class LeetCode_6 {
         }
 
         return outputStr.toString();
+    }
+
+    public String convert2 (String s, int numRows) {
+        if(numRows < 2) return s;
+        List<StringBuilder> rows = new ArrayList<>();
+        for(int i = 0; i < numRows; i++) rows.add(new StringBuilder());
+        int i = 0, flag = -1;
+        for(char c : s.toCharArray()) {
+            rows.get(i).append(c);
+            if(i == 0 || i == numRows -1) flag = - flag;
+            i += flag;
+        }
+        StringBuilder res = new StringBuilder();
+        for(StringBuilder row : rows) res.append(row);
+        return res.toString();
     }
 }
