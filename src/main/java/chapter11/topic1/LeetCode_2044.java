da1beec8bc8c63c34c441d79a22eb005e83dcb9d
Donald <448641125@qq.com>
2022-03-15 23:58:55 +0800

    Update: add leetcode 2044

---
+++ src/main/java/chapter11/topic1/LeetCode_2044.java
@@ -0,0 +1,28 @@
+package chapter11.topic1;
+
+/**
+ * @author donald
+ * @date 2022/03/15
+ */
+public class LeetCode_2044 {
+
+    public int countMaxOrSubsets(int[] nums) {
+        int maxOr = 0, cnt = 0;
+        for (int i = 0; i < 1 << nums.length; i++) {
+            int orVal = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if (((i >> j) & 1) == 1) {
+                    orVal |= nums[j];
+                }
+            }
+            if (orVal > maxOr) {
+                maxOr = orVal;
+                cnt = 1;
+            } else if (orVal == maxOr) {
+                cnt++;
+            }
+        }
+        return cnt;
+
+    }
+}
