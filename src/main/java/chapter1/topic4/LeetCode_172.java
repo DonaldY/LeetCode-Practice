657e7eb28d01f283cd47df88d663c3e2a6a82dc0
Donald <448641125@qq.com>
2022-03-25 23:27:24 +0800

    Update: update leetcode 172

--- src/main/java/chapter1/topic4/LeetCode_172.java
+++ src/main/java/chapter1/topic4/LeetCode_172.java
@@ -44,4 +44,15 @@ public class LeetCode_172 {
 
         return cnt;
     }
+
+    // Faster: 24.10%
+    public int trailingZeroes2(int n) {
+        int ans = 0;
+        for (int i = 5; i <= n; i += 5) {
+            for (int x = i; x % 5 == 0; x /= 5) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
 }
