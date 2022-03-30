90cdf4a9f0dc9c16f54342b797a7a2a1a6440ddd
Donald <448641125@qq.com>
2022-03-30 23:55:52 +0800

    Update: add leetcode 1606

---
+++ src/main/java/chapter9/topic1/LeetCode_1606.java
@@ -0,0 +1,41 @@
+package chapter9.topic1;
+
+import java.util.*;
+
+/**
+ * @author donald
+ * @date 2022/03/30
+ */
+public class LeetCode_1606 {
+    public List<Integer> busiestServers(int k, int[] arrival, int[] load) {
+        TreeSet<Integer> available = new TreeSet<>();
+        for (int i = 0; i < k; i++) {
+            available.add(i);
+        }
+        PriorityQueue<int[]> busy = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        int[] requests = new int[k];
+        for (int i = 0; i < arrival.length; i++) {
+            while (!busy.isEmpty() && busy.peek()[0] <= arrival[i]) {
+                available.add(busy.poll()[1]);
+            }
+            if (available.isEmpty()) {
+                continue;
+            }
+            Integer p = available.ceiling(i % k);
+            if (p == null) {
+                p = available.first();
+            }
+            requests[p]++;
+            busy.offer(new int[]{arrival[i] + load[i], p});
+            available.remove(p);
+        }
+        int maxRequest = Arrays.stream(requests).max().getAsInt();
+        List<Integer> ret = new ArrayList<>();
+        for (int i = 0; i < k; i++) {
+            if (requests[i] == maxRequest) {
+                ret.add(i);
+            }
+        }
+        return ret;
+    }
+}
