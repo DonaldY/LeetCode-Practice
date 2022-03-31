6c6fcd6faf50d89ca3909c6568f3ad53024a0d9b
Donald <448641125@qq.com>
2022-03-31 23:58:42 +0800

    Update: leetcode 728

---
+++ src/main/java/chapter4/topic3/LeetCode_728.java
@@ -0,0 +1,34 @@
+package chapter4.topic3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author donald
+ * @date 2022/03/31
+ */
+public class LeetCode_728 {
+
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> ans = new ArrayList<>();
+        for (int i = left; i <= right; i++) {
+            if (isSelfDividing(i)) {
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+
+    public boolean isSelfDividing(int num) {
+        int temp = num;
+        while (temp > 0) {
+            int digit = temp % 10;
+            if (digit == 0 || num % digit != 0) {
+                return false;
+            }
+            temp /= 10;
+        }
+        return true;
+    }
+
+}
