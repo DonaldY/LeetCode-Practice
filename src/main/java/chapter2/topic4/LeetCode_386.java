23524cc351897af38d6bcc243cd1cd52b40f3c35
Donald <448641125@qq.com>
2022-04-18 01:10:27 +0800

    Update: add leetcode 386

---
+++ src/main/java/chapter2/topic4/LeetCode_386.java
@@ -0,0 +1,48 @@
+package chapter2.topic4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author donald
+ * @date 2022/04/18
+ *
+ * 386. 字典序排数
+ *
+ * 给你一个整数 n ，按字典序返回范围 [1, n] 内所有整数。
+ *
+ * 你必须设计一个时间复杂度为 O(n) 且使用 O(1) 额外空间的算法。
+ *
+ * 示例 1：
+ *
+ * 输入：n = 13
+ * 输出：[1,10,11,12,13,2,3,4,5,6,7,8,9]
+ * 示例 2：
+ *
+ * 输入：n = 2
+ * 输出：[1,2]
+ *
+ *
+ * 题意： 按字典序输出
+ *
+ * 思路： DFS
+ */
+public class LeetCode_386 {
+
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> ret = new ArrayList<>();
+        int number = 1;
+        for (int i = 0; i < n; i++) {
+            ret.add(number);
+            if (number * 10 <= n) {
+                number *= 10;
+            } else {
+                while (number % 10 == 9 || number + 1 > n) {
+                    number /= 10;
+                }
+                number++;
+            }
+        }
+        return ret;
+    }
+}
