0f4d9b46f69cdec9f971f7938326a1784d249139
Donald <448641125@qq.com>
2022-04-23 00:06:32 +0800

    Update: add leetcode 396

--- src/main/java/chapter2/topic4/LeetCode_396.java
+++ src/main/java/chapter2/topic4/LeetCode_396.java
@@ -1,8 +1,24 @@
 package chapter2.topic4;
 
+import java.util.Arrays;
+
 /**
  * @author donald
  * @date 2022/04/23
  */
 public class LeetCode_396 {
+
+    public int maxRotateFunction(int[] nums) {
+        int f = 0, n = nums.length, numSum = Arrays.stream(nums).sum();
+        for (int i = 0; i < n; i++) {
+            f += i * nums[i];
+        }
+        int res = f;
+        for (int i = n - 1; i > 0; i--) {
+            f += numSum - n * nums[i];
+            res = Math.max(res, f);
+        }
+        return res;
+
+    }
 }
