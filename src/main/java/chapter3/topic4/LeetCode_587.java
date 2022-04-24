ad1b91d58600e877ca1e9bcbad3e512c68561f29
Donald <448641125@qq.com>
2022-04-24 09:10:17 +0800

    Update: add leetcode 587

---
+++ src/main/java/chapter3/topic4/LeetCode_587.java
@@ -0,0 +1,70 @@
+package chapter3.topic4;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author donald
+ * @date 2022/04/23
+ *
+ * 587. 安装栅栏
+ *
+ * 在一个二维的花园中，有一些用 (x, y) 坐标表示的树。由于安装费用十分昂贵，你的任务是先用最短的绳子围起所有的树。
+ * 只有当所有的树都被绳子包围时，花园才能围好栅栏。你需要找到正好位于栅栏边界上的树的坐标。
+ *
+ *
+ */
+public class LeetCode_587 {
+
+    public int[][] outerTrees(int[][] trees) {
+        int n = trees.length;
+        if (n < 4) {
+            return trees;
+        }
+        /* 按照 x 大小进行排序，如果 x 相同，则按照 y 的大小进行排序 */
+        Arrays.sort(trees, (a, b) -> {
+            if (a[0] == b[0]) {
+                return a[1] - b[1];
+            }
+            return a[0] - b[0];
+        });
+        List<Integer> hull = new ArrayList<>();
+        boolean[] used = new boolean[n];
+        /* hull[0] 需要入栈两次，不进行标记 */
+        hull.add(0);
+        /* 求出凸包的下半部分 */
+        for (int i = 1; i < n; i++) {
+            while (hull.size() > 1 && cross(trees[hull.get(hull.size() - 2)], trees[hull.get(hull.size() - 1)], trees[i]) < 0) {
+                used[hull.get(hull.size() - 1)] = false;
+                hull.remove(hull.size() - 1);
+            }
+            used[i] = true;
+            hull.add(i);
+        }
+        int m = hull.size();
+        /* 求出凸包的上半部分 */
+        for (int i = n - 2; i >= 0; i--) {
+            if (!used[i]) {
+                while (hull.size() > m && cross(trees[hull.get(hull.size() - 2)], trees[hull.get(hull.size() - 1)], trees[i]) < 0) {
+                    used[hull.get(hull.size() - 1)] = false;
+                    hull.remove(hull.size() - 1);
+                }
+                used[i] = true;
+                hull.add(i);
+            }
+        }
+        /* hull[0] 同时参与凸包的上半部分检测，因此需去掉重复的 hull[0] */
+        hull.remove(hull.size() - 1);
+        int size = hull.size();
+        int[][] res = new int[size][2];
+        for (int i = 0; i < size; i++) {
+            res[i] = trees[hull.get(i)];
+        }
+        return res;
+    }
+
+    public int cross(int[] p, int[] q, int[] r) {
+        return (q[0] - p[0]) * (r[1] - q[1]) - (q[1] - p[1]) * (r[0] - q[0]);
+    }
+}
