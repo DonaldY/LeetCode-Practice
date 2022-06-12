1f1957ecfd0b355b21ff2f8c3690cacbe24d91ce
Donald <448641125@qq.com>
2022-06-12 08:29:22 +0800

    Update: update leetcode 51

--- src/main/java/chapter1/topic2/LeetCode_51.java
+++ src/main/java/chapter1/topic2/LeetCode_51.java
@@ -6,10 +6,26 @@ import java.util.List;
 /**
  * @author donald
  * @date 2022/07/14
+ *
+ * 51. N 皇后
+ *
+ * 按照国际象棋的规则，皇后可以攻击与之处在同一行或同一列或同一斜线上的棋子。
+ *
+ * n 皇后问题 研究的是如何将 n 个皇后放置在 n×n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ *
+ * 给你一个整数 n ，返回所有不同的 n 皇后问题 的解决方案。
+ *
+ * 每一种解法包含一个不同的 n 皇后问题 的棋子放置方案，该方案中 'Q' 和 '.' 分别代表了皇后和空位。
+ *
+ * 输入：n = 4
+ * 输出：[[".Q..","...Q","Q...","..Q."],["..Q.","Q...","...Q",".Q.."]]
+ * 解释：如上图所示，4 皇后问题存在两个不同的解法。
+ *
+ * 思路：回溯思想
  */
 public class LeetCode_51 {
 
-        // Time: O(n!), Space: O(n^2), Faster: 99.80%
+    // Time: O(n!), Space: O(n^2), Faster: 99.80%
     public List<List<String>> solveNQueens(int n) {
         List<List<String>> result = new ArrayList<>();
         boolean[][] visited = new boolean[3][2*n];
