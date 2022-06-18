b2cb0d440f157d2094a2a847867d54608239f1a6
Donald <448641125@qq.com>
2022-06-18 08:29:22 +0800

    Update: add leetcode 52

---
+++ src/main/java/chapter1/topic2/LeetCode_52.java
@@ -0,0 +1,21 @@
+package chapter1.topic2;
+
+/**
+ * @author donald
+ * @date 2022/07/14
+ *
+ * 52. N皇后 II
+ *
+ * n 皇后问题 研究的是如何将 n 个皇后放置在 n × n 的棋盘上，并且使皇后彼此之间不能相互攻击。
+ *
+ * 给你一个整数 n ，返回 n 皇后问题 不同的解决方案的数量。
+ *
+ *
+ */
+public class LeetCode_52 {
+
+    public int totalNQueens(int n) {
+
+        return 0;
+    }
+}
