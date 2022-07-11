9728da31a658bd845a6db3664b96696e7d3ee4dd
Donald <448641125@qq.com>
2022-07-11 08:29:22 +0800

    Update: update leetcode 52

--- src/main/java/chapter1/topic2/LeetCode_52.java
+++ src/main/java/chapter1/topic2/LeetCode_52.java
@@ -1,5 +1,8 @@
 package chapter1.topic2;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author donald
  * @date 2022/07/14
@@ -10,12 +13,48 @@ package chapter1.topic2;
  *
  * 给你一个整数 n ，返回 n 皇后问题 不同的解决方案的数量。
  *
- *
+ * 思路：参考 LeetCode 51
  */
 public class LeetCode_52 {
 
+    // Time: O(n!), Space: O(n^2), Faster: 61.24%
     public int totalNQueens(int n) {
+        List<List<String>> result = new ArrayList<>();
+        boolean[][] visited = new boolean[3][2*n];
+        char[][] board = new char[n][n];
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                board[i][j] = '.';
+            }
+        }
 
-        return 0;
+        solve(0, n, result, board, visited);
+        return result.size();
+    }
+
+    private void solve(int row, int n, List<List<String>> result,
+                       char[][] board, boolean[][] visited) {
+        if (row == n) {
+            result.add(buildList(board));
+            return;
+        }
+        for (int col = 0; col < n; ++col) {
+            // 列、主对角线、副对角线
+            if (!visited[0][col] && !visited[1][row-col+n] && !visited[2][row+col]) {
+                board[row][col] = 'Q';
+                visited[0][col] = visited[1][row-col+n] = visited[2][row+col] = true;
+                solve(row+1, n, result, board, visited);
+                visited[0][col] = visited[1][row-col+n] = visited[2][row+col] = false;
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    private List<String> buildList(char[][] board) {
+        List<String> list = new ArrayList<>();
+        for (char[] row: board) {
+            list.add(new String(row));
+        }
+        return list;
     }
 }
