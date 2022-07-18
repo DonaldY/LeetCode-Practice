398464203166ba7dd5e4febadd5f2a408c5699f4
Donald <448641125@qq.com>
2022-07-18 08:34:36 +0800

    Update: update leetcode 200

--- src/main/java/chapter1/topic4/LeetCode_200.java
+++ src/main/java/chapter1/topic4/LeetCode_200.java
@@ -29,25 +29,18 @@ import java.util.Stack;
  *
  * 思路：
  * 1. DFS，遇到陆地则标记为水
- * 2. BFS
  */
 public class LeetCode_200 {
 
-    // Time: o(n * m), Space: o(1), Faster: 100.00%
+    // Time: o(n * m), Space: o(1), Faster: 61.27%
     public int numIslands(char[][] grid) {
-
         if (grid == null) return 0;
 
         int result = 0;
-
         for (int i = 0; i < grid.length; ++i) {
-
             for (int j = 0; j < grid[0].length; ++j) {
-
                 if (grid[i][j] == '1') {
-
                     findLands(grid, i, j);
-
                     ++result;
                 }
             }
@@ -65,7 +58,7 @@ public class LeetCode_200 {
         findLands(grid, n + 1, m);
         findLands(grid, n, m + 1);
         findLands(grid, n - 1, m);
-        findLands(grid, n, m -1);
+        findLands(grid, n, m - 1);
     }
 
     private void dfs(char[][] g, boolean[][] visit, int i, int j) {
@@ -73,13 +66,13 @@ public class LeetCode_200 {
         if (i < 0 || i >= m || j < 0 || j >= n || g[i][j] == '0' || visit[i][j])
             return;
         visit[i][j] = true;
-        dfs(g, visit, i-1, j);
-        dfs(g, visit, i+1, j);
-        dfs(g, visit, i, j-1);
-        dfs(g, visit, i, j+1);
+        dfs(g, visit, i - 1, j);
+        dfs(g, visit, i + 1, j);
+        dfs(g, visit, i, j - 1);
+        dfs(g, visit, i, j + 1);
     }
 
-    // Time: O(m*n), Space: O(m*n)
+    // Time: O(m * n), Space: O(m * n), Faster: 61.27%
     public int numberOfIslands(char[][] g) {
         if (g == null || g.length == 0 ||
                 g[0] == null || g[0].length == 0)
@@ -101,25 +94,26 @@ public class LeetCode_200 {
     ////////////////////////    DFS Iterative    ////////////////////////
     private class Point {
         int i, j;
+
         Point(int i, int j) {
             this.i = i;
             this.j = j;
         }
     }
 
-    private void dfsIterative(char[][] g, boolean[][] visit, int i , int j) {
+    private void dfsIterative(char[][] g, boolean[][] visit, int i, int j) {
         int m = g.length, n = g[0].length;
         Stack<Point> stack = new Stack<>();
         stack.push(new Point(i, j));
         while (!stack.isEmpty()) {
             Point p = stack.pop();
-            if (p.i < 0 || p.i >=m || p.j < 0 || p.j >= n || g[p.i][p.j] == '0' || visit[p.i][p.j])
+            if (p.i < 0 || p.i >= m || p.j < 0 || p.j >= n || g[p.i][p.j] == '0' || visit[p.i][p.j])
                 continue;
             visit[p.i][p.j] = true;
-            stack.push(new Point(p.i-1, p.j));
-            stack.push(new Point(p.i+1, p.j));
-            stack.push(new Point(p.i, p.j-1));
-            stack.push(new Point(p.i, p.j+1));
+            stack.push(new Point(p.i - 1, p.j));
+            stack.push(new Point(p.i + 1, p.j));
+            stack.push(new Point(p.i, p.j - 1));
+            stack.push(new Point(p.i, p.j + 1));
         }
     }
 
