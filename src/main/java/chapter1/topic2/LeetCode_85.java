ae22fd0be73c6540115610b7ca23ebe1b4ffdb72
Donald <448641125@qq.com>
2022-07-19 21:44:21 +0800

    Update: update leetcode 85

--- src/main/java/chapter1/topic2/LeetCode_85.java
+++ src/main/java/chapter1/topic2/LeetCode_85.java
@@ -29,16 +29,16 @@ package chapter1.topic2;
  */
 public class LeetCode_85 {
 
-    // Time: O(m * n), Space: O(n), Faster: 99.70%
+    // Time: O(m * n), Space: O(n), Faster: 99.85%
     public int maximalRectangle(char[][] matrix) {
         if (matrix == null || matrix.length == 0
-            || matrix[0] == null || matrix[0].length == 0)
+                || matrix[0] == null || matrix[0].length == 0)
             return 0;
         int m = matrix.length, n = matrix[0].length;
-        int [] heights = new int[n];
+        int[] heights = new int[n];
         int max = 0;
         for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n ; ++j) {
+            for (int j = 0; j < n; ++j) {
                 heights[j] = matrix[i][j] == '1' ? heights[j] + 1 : 0;
             }
             max = Math.max(max, largestRectangleHistogram(heights));
@@ -46,13 +46,13 @@ public class LeetCode_85 {
         return max;
     }
 
-    private int largestRectangleHistogram(int [] heights) {
+    private int largestRectangleHistogram(int[] heights) {
         int max = 0, n = heights.length, top = -1;
-        int [] st = new int[n + 1];
+        int[] st = new int[n + 1];
         for (int r = 0; r <= n; ++r) {
             int h = r == n ? 0 : heights[r];
             while (top != -1 && h < heights[st[top]]) {
-                int idx = st[top --];
+                int idx = st[top--];
                 int l = top == -1 ? -1 : st[top];
                 max = Math.max(max, heights[idx] * (r - l - 1));
             }
