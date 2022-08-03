5ec5c7a41a7d877e243c80f66530de3d081deb99
Donald <448641125@qq.com>
2022-08-03 16:55:27 +0800

    Update: update leetcode 621

--- src/main/java/chapter4/topic1/LeetCode_621.java
+++ src/main/java/chapter4/topic1/LeetCode_621.java
@@ -7,9 +7,31 @@ import java.util.Queue;
 /**
  * @author donald
  * @date 2022/08/03
+ *
+ * 这个题目说的是，给你一个字符数组和一个**非负整数** n。字符数组表示等待 CPU 处理的任务，每个任务用 A 到 Z 中的一个字符表示，并且每个任务都可以在一个时间单位内完成；n 表示冷却时间，即相同任务之间需要间隔至少 n 个时间单位才能再次执行，冷却时间内的每个时间单位，可以选择执行不同的任务或是让 CPU 处于闲置状态。
+ *
+ * 现在你要重新组织任务的执行顺序，并计算出最少需要多少个时间单位才能完成所有任务。
+ *
+ * # 比如说，给你的任务数组是：
+ * A, A, A, B, B, B, D
+ *
+ * # 给你的冷却时间是：
+ * n = 2
+ *
+ * # 完成所有任务最少需要 8 个时间单位，一种执行序列是：
+ * A, B, D, A, B, _, A, B
+ *
+ * 序列中的 '_' 表示 CPU 处于闲置状态。
+ *
+ * 因此，对于这个例子，你要返回 8。
+ *
+ * 思路：
+ * 1. 方法一：最大堆
+ * 2. 方法二：数学方法
  */
 public class LeetCode_621 {
 
+    // 方法一：最大堆
     // Time: O(m), Space: O(1), Faster: 35.62%
     public int leastIntervalMaxHeap(char[] tasks, int n) {
         if (tasks == null || tasks.length == 0) return 0;
@@ -24,7 +46,8 @@ public class LeetCode_621 {
             if (freq != 0) q.add(freq);
         }
 
-        int result = 0, idle = 0;
+        // 3. 遍历最大堆
+        int result = 0, idle = 0; // idle：最后一个分组有几个空闲
         while (!q.isEmpty()) {
             result += (n + 1);
             idle = n + 1 - q.size();
@@ -39,6 +62,7 @@ public class LeetCode_621 {
         return result - idle;
     }
 
+    // 方法二：数学方法
     // Time: O(m), Space: O(1), Faster: 100.00%
     public int leastIntervalMath(char[] tasks, int n) {
         if (tasks == null || tasks.length == 0) return 0;
