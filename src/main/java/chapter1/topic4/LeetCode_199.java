889f13f805b8c6b3e82d10a6a58e8b98c3ab5d26
Donald <448641125@qq.com>
2022-08-08 18:11:57 +0800

    Update: add leetcode 199

---
+++ src/main/java/chapter1/topic4/LeetCode_199.java
@@ -0,0 +1,49 @@
+package chapter1.topic4;
+
+import java.util.*;
+
+/**
+ * @author donald
+ * @date 2022/08/08
+ *
+ * 二叉树的右视图
+ *
+ * 这个题目说的是，给你一棵二叉树，并且你站在这棵树的右边，你要返回从上到下看到的节点值。
+ * 比如说，给你的二叉树是：
+ *
+ *      1
+ *    /   \
+ *   2     4
+ *  / \
+ * 6   8
+ *
+ * 站在这棵二叉树的右边看过来，从上到下看到的数字依次是：
+ *
+ * [1, 4, 8]
+ *
+ * 思路：
+ * 1. BFS： 层序遍历即可，右边数字
+ * 2. DFS
+ */
+public class LeetCode_199 {
+
+    // 方法一： BFS
+    // Time: O(n), Space: O(log(n)), Faster: 82.03%
+    public List<Integer> rightSideViewBFS(TreeNode root) {
+        if (null == root) return Collections.emptyList();
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<Integer> result = new ArrayList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            TreeNode node = null;
+            while (size-- > 0) {
+                node = queue.poll();
+                if (null != node.left) queue.add(node.left);
+                if (null != node.right) queue.add(node.right);
+            }
+            result.add(node.val);
+        }
+        return result;
+    }
+}
