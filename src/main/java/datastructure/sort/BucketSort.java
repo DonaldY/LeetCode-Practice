b3d265dff220a59492db11954b40ec3eac1cc533
Donald <448641125@qq.com>
2022-08-12 18:14:00 +0800

    Update: add bucketSort

--- src/main/java/datastructure/sort/BucketSort.java
+++ src/main/java/datastructure/sort/BucketSort.java
@@ -19,26 +19,30 @@ public class BucketSort {
     public void sort(int[] arr) {
         if (arr == null || arr.length == 0) return;
         int max = arr[0], min = arr[0];
+        // 计算最大值和最小值
         for (int num : arr) {
             if (num > max) max = num;
             if (num < min) min = num;
         }
 
+        // 桶的数量 = 数组长度 / 桶大小
         int bucketCount = arr.length / bucketSize;
         List<List<Integer>> buckets = new ArrayList<>(bucketCount);
         for (int i = 0; i < bucketCount; ++i)
             buckets.add(new ArrayList<>());
 
         for (int num : arr) {
+            // 计算放在哪个桶中
             int idx = (int) ((num - min) / (max - min + 1.0) * bucketCount);
             buckets.get(idx).add(num);
         }
 
         int idx = 0;
         for (List<Integer> bucket : buckets) {
-            insertionSort(bucket);
-            for (int num : bucket)
+            insertionSort(bucket);   // 桶内：插入排序
+            for (int num : bucket) { // 再放入结果中
                 arr[idx++] = num;
+            }
         }
     }
 
@@ -47,10 +51,11 @@ public class BucketSort {
         for (int i = 1; i < arr.size(); ++i) {
             int cur = arr.get(i);
             int j = i - 1;
-            while (j >= 0 && arr.get(j) > cur) {
+            while (j >= 0 && arr.get(j) > cur) { // 先找一个合适的位置
                 arr.set(j + 1, arr.get(j));
                 --j;
             }
+            // 找到合适的位置，放下
             arr.set(j + 1, cur);
         }
     }
