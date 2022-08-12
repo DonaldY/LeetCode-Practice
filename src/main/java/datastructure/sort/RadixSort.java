009e85db92aab69a35d008df3ba2e50378529c43
Donald <448641125@qq.com>
2022-08-13 02:10:49 +0800

    Update: update RedixSort

--- src/main/java/datastructure/sort/RadixSort.java
+++ src/main/java/datastructure/sort/RadixSort.java
@@ -7,6 +7,20 @@ import java.util.Arrays;
  * @date 2022/08/13
  */
 public class RadixSort {
+
+    // 基数为 256，每次取 8 个二进制位作为一个部分进行处理，32 位整数需要处理 4 次。
+    // 每次取出的 8 个二进制位会作为计数排序的键值，去排序原始数据。
+    // 每次处理 8 个二进制位，是时间/空间上比较折衷的方法。
+    // 如果一次处理 16 个二进制位，速度会稍微快一些。但需要额外的空间是 2^16 = 65536，远大于每次处理 8 个二进制位所需空间。
+    // 如果一次只处理 4 个二进制位，速度则会慢很多。
+    public void sort4pass(int[] arr) {
+        sort(arr, 8, 0xff);
+    }
+
+    public void sort8pass(int[] arr) {
+        sort(arr, 4, 0x0f);
+    }
+
     // Time: O(32/b * n), Space: O(n + 2^b)
     private void sort(int[] arr, int bits, int mask) {
         if (arr == null || arr.length == 0) return;
@@ -43,18 +57,5 @@ public class RadixSort {
         System.arraycopy(arr, len, tmp, 0, n - len); // copy negative part to tmp
         System.arraycopy(arr, 0, tmp, n - len, len); // copy positive part to tmp
         System.arraycopy(tmp, 0, arr, 0, n); // copy back to arr
-    }
-
-    // 基数为 256，每次取 8 个二进制位作为一个部分进行处理，32 位整数需要处理 4 次。
-    // 每次取出的 8 个二进制位会作为计数排序的键值，去排序原始数据。
-    // 每次处理 8 个二进制位，是时间/空间上比较折衷的方法。
-    // 如果一次处理 16 个二进制位，速度会稍微快一些。但需要额外的空间是 2^16 = 65536，远大于每次处理 8 个二进制位所需空间。
-    // 如果一次只处理 4 个二进制位，速度则会慢很多。
-    public void sort4pass(int[] arr) {
-        sort(arr, 8, 0xff);
-    }
-
-    public void sort8pass(int[] arr) {
-        sort(arr, 4, 0x0f);
     }
 }
