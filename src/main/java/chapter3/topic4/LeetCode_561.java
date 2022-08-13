b12290e151f95199722f0b8980672dee2a9daea1
Donald <448641125@qq.com>
2022-08-13 13:43:42 +0800

    Update: add leetcode 561

---
+++ src/main/java/chapter3/topic4/LeetCode_561.java
@@ -0,0 +1,45 @@
+package chapter3.topic4;
+
+import java.util.Arrays;
+
+/**
+ * @author donald
+ * @date 2022/08/13
+ *
+ * 给定长度为 2n 的整数数组 nums ，你的任务是将这些数分成 n 对, 例如 (a1, b1), (a2, b2), ..., (an, bn) ，使得从 1 到 n 的 min(ai, bi) 总和最大。
+ *
+ * 返回该 最大总和 。
+ *
+ * 示例 1：
+ * 输入：nums = [1,4,3,2]
+ * 输出：4
+ * 解释：所有可能的分法（忽略元素顺序）为：
+ * 1. (1, 4), (2, 3) -> min(1, 4) + min(2, 3) = 1 + 2 = 3
+ * 2. (1, 3), (2, 4) -> min(1, 3) + min(2, 4) = 1 + 2 = 3
+ * 3. (1, 2), (3, 4) -> min(1, 2) + min(3, 4) = 1 + 3 = 4
+ * 所以最大总和为 4
+ *
+ * 示例 2：
+ * 输入：nums = [6,2,6,5,1,2]
+ * 输出：9
+ * 解释：最优的分法为 (2, 1), (2, 5), (6, 6). min(2, 1) + min(2, 5) + min(6, 6) = 1 + 2 + 6 = 9
+ *
+ * 题意： 从最小数开始匹配（两两匹配），再取最小值，最后求和
+ *
+ * 思路：
+ * 1. 暴力法： 排序后，隔一个添加
+ * 2.
+ */
+public class LeetCode_561 {
+
+    // Time: O(), Space: O(1), Faster: 9.26%
+    public int arrayPairSum(int[] nums) {
+        if (null == nums || nums.length == 0) return -1;
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            sum += nums[i];
+        }
+        return sum;
+    }
+}
