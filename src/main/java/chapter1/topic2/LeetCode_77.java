31bcecdeed9a74423a8f0ae3e64d1c61e287acf3
Donald <448641125@qq.com>
2022-08-23 00:35:40 +0800

    Update: update leetcode 77

--- src/main/java/chapter1/topic2/LeetCode_77.java
+++ src/main/java/chapter1/topic2/LeetCode_77.java
@@ -1,6 +1,7 @@
 package chapter1.topic2;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -62,10 +63,10 @@ public class LeetCode_77 {
         if (k == 0) {
             result.add(new ArrayList<>(elem));
         } else {
-            for (int i = start; i <= n-k+1; ++i) {
+            for (int i = start; i <= n - k + 1; ++i) {
                 elem.add(i);
-                combine(n, k-1, i+1, elem, result);
-                elem.remove(elem.size()-1);
+                combine(n, k - 1, i + 1, elem, result);
+                elem.remove(elem.size() - 1);
             }
         }
     }
@@ -76,6 +77,26 @@ public class LeetCode_77 {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> elem = new ArrayList<>();
         combine(n, k, 1, elem, result);
+        return result;
+    }
+
+    // Time: O(k*C(n, k)), Space: O(k*C(n, k))
+    public List<List<Integer>> combineIterative(int n, int k) {
+        if (k > n) return Collections.emptyList();
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 1; i <= n - k + 1; ++i) result.add(Arrays.asList(i));
+        for (int i = 2; i <= k; ++i) {
+            List<List<Integer>> tmp = new ArrayList<>();
+            for (List<Integer> elem : result) {
+                int lastNum = elem.get(elem.size() - 1);
+                for (int j = lastNum + 1; j <= n - k + i; ++j) {
+                    List<Integer> copy = new ArrayList<>(elem);
+                    copy.add(j);
+                    tmp.add(copy);
+                }
+            }
+            result = tmp;
+        }
         return result;
     }
 }
