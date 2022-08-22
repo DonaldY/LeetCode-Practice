d6a6324388e80a5429084f9778d37a89123bd8f2
Donald <448641125@qq.com>
2022-08-23 00:34:58 +0800

    Update: update leetcode 77

--- src/main/java/chapter1/topic2/LeetCode_77.java
+++ src/main/java/chapter1/topic2/LeetCode_77.java
@@ -55,4 +55,27 @@ public class LeetCode_77 {
             arr.remove(arr.size() - 1);
         }
     }
+
+
+    private void combine(int n, int k, int start,
+                         List<Integer> elem, List<List<Integer>> result) {
+        if (k == 0) {
+            result.add(new ArrayList<>(elem));
+        } else {
+            for (int i = start; i <= n-k+1; ++i) {
+                elem.add(i);
+                combine(n, k-1, i+1, elem, result);
+                elem.remove(elem.size()-1);
+            }
+        }
+    }
+
+    // Time: O(k*C(n, k)), Space: O(k)
+    public List<List<Integer>> combineRecursive(int n, int k) {
+        if (k > n) return Collections.emptyList();
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> elem = new ArrayList<>();
+        combine(n, k, 1, elem, result);
+        return result;
+    }
 }
