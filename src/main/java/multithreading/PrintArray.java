4815285bdbe30d3a2b154c5b4ddf68b07a7943ca
Donald <448641125@qq.com>
2022-09-10 02:55:28 +0800

    Update: update printArray

--- src/main/java/multithreading/PrintArray.java
+++ src/main/java/multithreading/PrintArray.java
@@ -8,7 +8,7 @@ public class PrintArray implements Runnable {
 
     static int num = 0;
     static final Object lock = new Object();
-    public static int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30};
+    public static int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
 
     public static void main(String[] args) throws InterruptedException {
 
@@ -19,25 +19,24 @@ public class PrintArray implements Runnable {
         thread2.start();
         thread3.start();
 
-        Thread.sleep(10000);
+        Thread.sleep(5000);
     }
 
     @Override
     public void run() {
 
-        while (num < arr.length) {
-
-             synchronized (lock) {
-
+        synchronized (lock) {
+            while (num < arr.length) {
                 System.out.println(Thread.currentThread().getName() + " print num : " + arr[num]);
                 ++num;
                 lock.notify();
-                 try {
-                     lock.wait();
-                 } catch (InterruptedException e) {
-                     e.printStackTrace();
-                 }
-             }
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            lock.notify();
         }
     }
 }
