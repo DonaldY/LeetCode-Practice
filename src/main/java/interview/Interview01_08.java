5a18aaa842b61414f7970ab3a46dc38ffbc76273
Donald <448641125@qq.com>
2022-09-30 00:26:06 +0800

    add interview 0108

---
+++ src/main/java/interview/Interview01_08.java
@@ -0,0 +1,70 @@
+package interview;
+
+/**
+ * @author donald
+ * @date 2022/09/30
+ *
+ * 面试题 01.08. 零矩阵
+ *
+ * 编写一种算法，若M × N矩阵中某个元素为0，则将其所在的行与列清零。
+ *
+ *
+ * 示例 1：
+ * 输入：
+ * [
+ *   [1,1,1],
+ *   [1,0,1],
+ *   [1,1,1]
+ * ]
+ * 输出：
+ * [
+ *   [1,0,1],
+ *   [0,0,0],
+ *   [1,0,1]
+ * ]
+ *
+ * 示例 2：
+ * 输入：
+ * [
+ *   [0,1,2,0],
+ *   [3,4,5,2],
+ *   [1,3,1,5]
+ * ]
+ * 输出：
+ * [
+ *   [0,0,0,0],
+ *   [0,4,5,0],
+ *   [0,3,1,0]
+ * ]
+ *
+ * 题意： 清零判断, 需要记录原先 0 的位置
+ *
+ * 思路：
+ * 1. 暴力法： 一个个判断,
+ */
+public class Interview01_08 {
+
+    // 方法一： 暴力法
+    // Time: O(n^2), Space: O(1), Faster: 43.66%
+    public void setZeroes(int[][] matrix) {
+        if (null == matrix || matrix.length == 0) return;
+        boolean [][] flagMatrix = new boolean[matrix.length][matrix[0].length];
+        for (int i = 0; i < matrix.length; ++i) {
+            for (int j = 0; j < matrix[0].length; ++j) {
+                if (matrix[i][j] == 0) {
+                    flagMatrix[i][j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < matrix.length; ++i) {
+            for (int j = 0; j < matrix[0].length; ++j) {
+                // 原始的值为0, 而不是后来被赋予的
+                if (flagMatrix[i][j]) {
+                    for (int k = 0; k < matrix.length; ++k) matrix[k][j] = 0;
+                    for (int k = 0; k < matrix[0].length; ++k) matrix[i][k] = 0;
+                }
+            }
+        }
+    }
+
+}
