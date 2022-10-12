3353bc357b3baa9e73dbe5388405d559e68d6a9f
Donald <448641125@qq.com>
2022-10-12 18:29:41 +0800

    Update: add leetcode 953

---
+++ src/main/java/chapter5/topic4/LeetCode_953.java
@@ -0,0 +1,17 @@
+package chapter5.topic4;
+
+/**
+ * @author donald
+ * @date 2022/10/12
+ *
+ * 题意：
+ *
+ * 思路：
+ */
+public class LeetCode_953 {
+
+    // Time: O(), Space: O(), Faster:
+    public boolean isAlienSorted(String[] words, String order) {
+        return false;
+    }
+}
