c30fbf0041cb26d53c705e48eb02d78e5b71f92c
Donald <448641125@qq.com>
2022-10-16 12:44:23 +0800

    Update: add leetcode 729

--- src/main/java/chapter4/topic3/LeetCode_729.java
+++ src/main/java/chapter4/topic3/LeetCode_729.java
@@ -1,5 +1,8 @@
 package chapter4.topic3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author donald
  * @date 2022/10/14
@@ -7,13 +10,22 @@ package chapter4.topic3;
 public class LeetCode_729 {
 }
 
+// 方法一： 暴力法
 class MyCalendar {
 
+    private List<int[]> schedules;
     public MyCalendar() {
-
+        this.schedules = new ArrayList<>();
     }
 
+    // Time: O(n), Faster: 34.29%
     public boolean book(int start, int end) {
+        for (int[] schedule : this.schedules) {
+            if (schedule[0] < end && start < schedule[1]) {
+                return false;
+            }
+        }
+        schedules.add(new int[] {start, end});
         return true;
     }
 }
