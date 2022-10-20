c228a15f4df636741d40b83fb4be785bc4ddf8cb
Donald <448641125@qq.com>
2022-10-20 17:00:14 +0800

    Update: update leetcode 1122

--- src/main/java/chapter6/topic3/LeetCode_1122.java
+++ src/main/java/chapter6/topic3/LeetCode_1122.java
@@ -9,7 +9,7 @@ package chapter6.topic3;
  */
 public class LeetCode_1122 {
 
-    // Time: O(n), Space: O(n), Faster:
+    // Time: O(n), Space: O(n), Faster: 100.00%
     public int[] relativeSortArray(int[] arr1, int[] arr2) {
         int n = 1000;
 
@@ -20,9 +20,9 @@ public class LeetCode_1122 {
         int[] result = new int[arr1.length];
         int idx = 0;
         // 2. 按照 arr2 放置
-        for (int i = 0; i < arr2.length; ++i) {
-            while (map[i]-- > 0) {
-                result[idx++] = i;
+        for (int num : arr2) {
+            while (map[num]-- > 0) {
+                result[idx++] = num;
             }
         }
 
