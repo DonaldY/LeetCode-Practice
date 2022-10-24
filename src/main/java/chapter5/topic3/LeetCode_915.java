dd3bd9b0c092cfdd59900c8bb7d85920a9348840
Donald <448641125@qq.com>
2022-10-24 23:54:01 +0800

    Update: add leetcode 512

---
+++ src/main/java/chapter5/topic3/LeetCode_915.java
@@ -0,0 +1,16 @@
+package chapter5.topic3;
+
+/**
+ * @author donald
+ * @date 2022/10/24
+ *
+ *
+ * 思路：
+ * 1.
+ */
+public class LeetCode_915 {
+
+    public int partitionDisjoint(int[] nums) {
+        return 0;
+    }
+}
