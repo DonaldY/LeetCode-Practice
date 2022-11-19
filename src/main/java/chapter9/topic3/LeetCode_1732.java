30de1ac6d28df082dd70b289b77c1dcf68beb12a
Donald <448641125@qq.com>
2022-11-19 23:50:14 +0800

    Update: add leetcode 1732

--- src/main/java/chapter9/topic3/LeetCode_1732.java
+++ src/main/java/chapter9/topic3/LeetCode_1732.java
@@ -5,4 +5,13 @@ package chapter9.topic3;
  * @date 2022/11/19
  */
 public class LeetCode_1732 {
+    // Time: O(n), Space: O(1), Faster: 100.00%
+    public int largestAltitude(int[] gain) {
+        int ans = 0, cur = 0;
+        for (int i = 0; i < gain.length; ++i) {
+            cur += gain[i];
+            ans = Math.max(ans, cur);
+        }
+        return ans;
+    }
 }
