05a5c9fd832cfa9c5f0d230c2a283296e2b7a58b
Donald <448641125@qq.com>
2022-11-20 09:20:17 +0800

    Update: add leetcode 799

---
+++ src/main/java/chapter4/topic4/LeetCode_799.java
@@ -0,0 +1,25 @@
+package chapter4.topic4;
+
+/**
+ * @author donald
+ * @date 2022/11/20
+ */
+public class LeetCode_799 {
+
+    // Time: O(), Space: O(), Faster:
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        double[] row = {poured};
+        for (int i = 1; i <= query_row; i++) {
+            double[] nextRow = new double[i + 1];
+            for (int j = 0; j < i; j++) {
+                double volume = row[j];
+                if (volume > 1) {
+                    nextRow[j] += (volume - 1) / 2;
+                    nextRow[j + 1] += (volume - 1) / 2;
+                }
+            }
+            row = nextRow;
+        }
+        return Math.min(1, row[query_glass]);
+    }
+}
