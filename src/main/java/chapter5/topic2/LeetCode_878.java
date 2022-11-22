613dbe34a34217497f3402ce5625b6cb9983d890
Donald <448641125@qq.com>
2022-11-22 13:34:27 +0800

    Update: add leetcode 878

---
+++ src/main/java/chapter5/topic2/LeetCode_878.java
@@ -0,0 +1,33 @@
+package chapter5.topic2;
+
+/**
+ * @author donald
+ * @date 2022/11/22
+ */
+public class LeetCode_878 {
+    static final int MOD = 1000000007;
+
+    public int nthMagicalNumber(int n, int a, int b) {
+        long l = Math.min(a, b);
+        long r = (long) n * Math.min(a, b);
+        int c = lcm(a, b);
+        while (l <= r) {
+            long mid = (l + r) / 2;
+            long cnt = mid / a + mid / b - mid / c;
+            if (cnt >= n) {
+                r = mid - 1;
+            } else {
+                l = mid + 1;
+            }
+        }
+        return (int) ((r + 1) % MOD);
+    }
+
+    public int lcm(int a, int b) {
+        return a * b / gcd(a, b);
+    }
+
+    public int gcd(int a, int b) {
+        return b != 0 ? gcd(b, a % b) : a;
+    }
+}
