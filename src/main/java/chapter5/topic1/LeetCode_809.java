3cfc43bdd7530e327ac8d141b6c424633a5c51b0
Donald <448641125@qq.com>
2022-11-25 23:36:59 +0800

    Update: add leetcode 809

---
+++ src/main/java/chapter5/topic1/LeetCode_809.java
@@ -0,0 +1,45 @@
+package chapter5.topic1;
+
+/**
+ * @author donald
+ * @date 2022/11/25
+ */
+public class LeetCode_809 {
+    // Faster: 97.85%
+    public int expressiveWords(String s, String[] words) {
+        int ans = 0;
+        for (String word : words) {
+            if (expand(s, word)) {
+                ++ans;
+            }
+        }
+        return ans;
+    }
+
+    private boolean expand(String s, String t) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < t.length()) {
+            if (s.charAt(i) != t.charAt(j)) {
+                return false;
+            }
+            char ch = s.charAt(i);
+            int cnti = 0;
+            while (i < s.length() && s.charAt(i) == ch) {
+                ++cnti;
+                ++i;
+            }
+            int cntj = 0;
+            while (j < t.length() && t.charAt(j) == ch) {
+                ++cntj;
+                ++j;
+            }
+            if (cnti < cntj) {
+                return false;
+            }
+            if (cnti != cntj && cnti < 3) {
+                return false;
+            }
+        }
+        return i == s.length() && j == t.length();
+    }
+}
