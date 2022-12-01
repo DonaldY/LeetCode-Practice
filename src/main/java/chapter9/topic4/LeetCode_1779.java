2145447d3ca4e72658ec60a9ae68d666c70d14b5
Donald <448641125@qq.com>
2022-12-02 00:12:18 +0800

    Update: add leetcode 1779

--- src/main/java/chapter9/topic4/LeetCode_1779.java
+++ src/main/java/chapter9/topic4/LeetCode_1779.java
@@ -5,4 +5,26 @@ package chapter9.topic4;
  * @date 2022/12/01
  */
 public class LeetCode_1779 {
+
+    // Time: O(n), Space: O(1), Faster: 100%
+    public int nearestValidPoint(int x, int y, int[][] points) {
+        if (null == points) return -1;
+        int ans = -1, min = Integer.MAX_VALUE;
+        for (int i = 0 ; i < points.length; ++i) {
+            if (points[i][0] == x) {
+                int dist = Math.abs(y - points[i][1]);
+                if (dist < min) {
+                    min = dist;
+                    ans = i;
+                }
+            } else if (points[i][1] == y) {
+                int dist = Math.abs(x - points[i][0]);
+                if (dist < min) {
+                    min = dist;
+                    ans = i;
+                }
+            }
+        }
+        return ans;
+    }
 }
