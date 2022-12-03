6a494e96a8e049c499755746d203e9808585ddbf
Donald <448641125@qq.com>
2022-12-03 23:59:31 +0800

    Update: add leetcode 1796

---
+++ src/main/java/chapter9/topic4/LeetCode_1796.java
@@ -0,0 +1,24 @@
+package chapter9.topic4;
+
+/**
+ * @author donald
+ * @date 2022/12/03
+ */
+public class LeetCode_1796 {
+    public int secondHighest(String s) {
+        int first = -1, second = -1;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (Character.isDigit(c)) {
+                int num = c - '0';
+                if (num > first) {
+                    second = first;
+                    first = num;
+                } else if (num < first && num > second) {
+                    second = num;
+                }
+            }
+        }
+        return second;
+    }
+}
