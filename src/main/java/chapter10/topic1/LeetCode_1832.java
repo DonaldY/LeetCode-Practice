0a57268fef9b43259cc045fe05cf86742a2d2126
Donald <448641125@qq.com>
2022-12-13 23:49:29 +0800

    Update: add leetcode 1832

---
+++ src/main/java/chapter10/topic1/LeetCode_1832.java
@@ -0,0 +1,21 @@
+package chapter10.topic1;
+
+/**
+ * @author donald
+ * @date 2022/12/13
+ */
+public class LeetCode_1832 {
+    public boolean checkIfPangram(String sentence) {
+        boolean[] exist = new boolean[26];
+        for (int i = 0; i < sentence.length(); i++) {
+            char c = sentence.charAt(i);
+            exist[c - 'a'] = true;
+        }
+        for (boolean x : exist) {
+            if (!x) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
