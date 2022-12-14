5591a6c95f9938f20f906f31f97e35dc43c3248a
Donald <448641125@qq.com>
2022-12-14 23:30:50 +0800

    Update: add leetcode 1697

---
+++ src/main/java/chapter9/topic2/LeetCode_1697.java
@@ -0,0 +1,47 @@
+package chapter9.topic2;
+
+import java.util.Arrays;
+
+/**
+ * @author donald
+ * @date 2022/12/14
+ */
+public class LeetCode_1697 {
+    public boolean[] distanceLimitedPathsExist(int n, int[][] edgeList, int[][] queries) {
+        Arrays.sort(edgeList, (a, b) -> a[2] - b[2]);
+
+        Integer[] index = new Integer[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            index[i] = i;
+        }
+        Arrays.sort(index, (a, b) -> queries[a][2] - queries[b][2]);
+
+        int[] uf = new int[n];
+        for (int i = 0; i < n; i++) {
+            uf[i] = i;
+        }
+        boolean[] res = new boolean[queries.length];
+        int k = 0;
+        for (int i : index) {
+            while (k < edgeList.length && edgeList[k][2] < queries[i][2]) {
+                merge(uf, edgeList[k][0], edgeList[k][1]);
+                k++;
+            }
+            res[i] = find(uf, queries[i][0]) == find(uf, queries[i][1]);
+        }
+        return res;
+    }
+
+    public int find(int[] uf, int x) {
+        if (uf[x] == x) {
+            return x;
+        }
+        return uf[x] = find(uf, uf[x]);
+    }
+
+    public void merge(int[] uf, int x, int y) {
+        x = find(uf, x);
+        y = find(uf, y);
+        uf[y] = x;
+    }
+}
