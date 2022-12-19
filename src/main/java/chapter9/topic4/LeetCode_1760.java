0c6e02ae87d464b74238fb8aa65ea2e2cdef92e5
Donald <448641125@qq.com>
2022-12-20 00:35:59 +0800

    Update: add leetcode 1760

---
+++ src/main/java/chapter9/topic4/LeetCode_1760.java
@@ -0,0 +1,29 @@
+package chapter9.topic4;
+
+import java.util.Arrays;
+
+/**
+ * @author donald
+ * @date 2022/12/20
+ */
+public class LeetCode_1760 {
+
+    public int minimumSize(int[] nums, int maxOperations) {
+        int left = 1, right = Arrays.stream(nums).max().getAsInt();
+        int ans = 0;
+        while (left <= right) {
+            int y = (left + right) / 2;
+            long ops = 0;
+            for (int x : nums) {
+                ops += (x - 1) / y;
+            }
+            if (ops <= maxOperations) {
+                ans = y;
+                right = y - 1;
+            } else {
+                left = y + 1;
+            }
+        }
+        return ans;
+    }
+}
