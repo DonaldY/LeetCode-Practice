0dd1492500a14418e13e334fccdc4c6a2c979c15
Donald <448641125@qq.com>
2022-12-19 23:56:37 +0800

    Update: add leetcode 1971

--- src/main/java/chapter10/topic4/LeetCode_1971.java
+++ src/main/java/chapter10/topic4/LeetCode_1971.java
@@ -1,5 +1,10 @@
 package chapter10.topic4;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * @author donald
  * @date 2022/12/19
@@ -36,5 +41,34 @@ public class LeetCode_1971 {
         }
 
         return false;
+    }
+
+    public boolean validPathBFS(int n, int[][] edges, int source, int destination) {
+        List<Integer>[] adj = new List[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        for (int[] edge : edges) {
+            int x = edge[0], y = edge[1];
+            adj[x].add(y);
+            adj[y].add(x);
+        }
+        boolean[] visited = new boolean[n];
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.offer(source);
+        visited[source] = true;
+        while (!queue.isEmpty()) {
+            int vertex = queue.poll();
+            if (vertex == destination) {
+                break;
+            }
+            for (int next : adj[vertex]) {
+                if (!visited[next]) {
+                    queue.offer(next);
+                    visited[next] = true;
+                }
+            }
+        }
+        return visited[destination];
     }
 }
