9fb5eefa5f5d80924414f8bc7c1a017f2828fc82
Donald <448641125@qq.com>
2022-12-20 00:32:45 +0800

    Update: add leetcode 1971

--- src/main/java/chapter10/topic4/LeetCode_1971.java
+++ src/main/java/chapter10/topic4/LeetCode_1971.java
@@ -11,37 +11,42 @@ import java.util.Queue;
  */
 public class LeetCode_1971 {
 
-    // Time: O(), Space: O(), Faster:
+    // DFS: 无向图，可以双向
+    // Time: O(n + m), Space: O(n + m), Faster: 100%
     public boolean validPath(int n, int[][] edges, int source, int destination) {
 
-        if (null == edges || edges.length == 0) return true;
-
-        boolean[] flag = new boolean[n];
-
-        return isValidPath(n, edges, source, destination, flag);
-    }
-
-    private boolean isValidPath(int n, int[][] edges, int source, int destination, boolean[] flag) {
-
-        for (int i = 0; i < edges.length; ++i) {
-            if (flag[i]) {
-
-                continue;
-            }
-            if (edges[i][1] == destination) {
-
-                return true;
-            }
-            flag[i] = true;
-            boolean isValidPath = isValidPath(n, edges, edges[i][1], destination, flag);
-            if (isValidPath) {
-                return true;
-            }
-            flag[i] = false;
+        List<Integer>[] adj = new List[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        for (int[] edge : edges) {
+            int x = edge[0], y = edge[1];
+            adj[x].add(y);
+            adj[y].add(x);
         }
 
+        boolean[] visited = new boolean[n];
+
+        return dfs(source, destination, adj, visited);
+    }
+
+    public boolean dfs(int source, int destination, List<Integer>[] adj, boolean[] visited) {
+        if (source == destination) {
+
+            return true;
+        }
+
+        visited[source] = true;
+
+        for (int next : adj[source]) {
+
+            if (!visited[next] && dfs(next, destination, adj, visited)) {
+                return true;
+            }
+        }
         return false;
     }
+
 
     // BFS
     // Time: O(n + m), Space: O(n + m), Faster: 100%
