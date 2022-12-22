2dcf2bcd7b75a19ccccd744636e45a5de6f64944
Donald <448641125@qq.com>
2022-12-22 23:18:32 +0800

    Update: add leetcode 1799

---
+++ src/main/java/chapter9/topic4/LeetCode_1799.java
@@ -0,0 +1,45 @@
+package chapter9.topic4;
+
+/**
+ * @author donald
+ * @date 2022/12/22
+ */
+public class LeetCode_1799 {
+
+    public int maxScore(int[] nums) {
+        int m = nums.length;
+        int[] dp = new int[1 << m];
+        int[][] gcdTmp = new int[m][m];
+        for (int i = 0; i < m; ++i) {
+            for (int j = i + 1; j < m; ++j) {
+                gcdTmp[i][j] = gcd(nums[i], nums[j]);
+            }
+        }
+        int all = 1 << m;
+        for (int s = 1; s < all; ++s) {
+            int t = Integer.bitCount(s);
+            if ((t & 1) != 0) {
+                continue;
+            }
+            for (int i = 0; i < m; ++i) {
+                if (((s >> i) & 1) != 0) {
+                    for (int j = i + 1; j < m; ++j) {
+                        if (((s >> j) & 1) != 0) {
+                            dp[s] = Math.max(dp[s], dp[s ^ (1 << i) ^ (1 << j)] + t / 2 * gcdTmp[i][j]);
+                        }
+                    }
+                }
+            }
+        }
+        return dp[all - 1];
+    }
+
+    public int gcd(int num1, int num2) {
+        while (num2 != 0) {
+            int temp = num1;
+            num1 = num2;
+            num2 = temp % num2;
+        }
+        return num1;
+    }
+}
