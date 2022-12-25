2a4d0daaf9ee6d00bacdbf255a46dd6dfb213392
Donald <448641125@qq.com>
2022-12-25 23:37:50 +0800

    Update: add leetcode 1739

---
+++ src/main/java/chapter9/topic3/LeetCode_1739.java
@@ -0,0 +1,23 @@
+package chapter9.topic3;
+
+/**
+ * @author donald
+ * @date 2022/12/25
+ */
+public class LeetCode_1739 {
+    public int minimumBoxes(int n) {
+        int cur = 1, i = 1, j = 1;
+        while (n > cur) {
+            n -= cur;
+            i++;
+            cur += i;
+        }
+        cur = 1;
+        while (n > cur) {
+            n -= cur;
+            j++;
+            cur++;
+        }
+        return (i - 1) * i / 2 + j;
+    }
+}
