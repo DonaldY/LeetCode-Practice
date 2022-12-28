3dfb9cb73a64abc12bab895c9698fd87b6f67a8c
Donald <448641125@qq.com>
2022-12-28 23:58:09 +0800

    Update: add leetcode 1750

---
+++ src/main/java/chapter9/topic4/LeetCode_1750.java
@@ -0,0 +1,22 @@
+package chapter9.topic4;
+
+/**
+ * @author donald
+ * @date 2022/12/28
+ */
+public class LeetCode_1750 {
+    public int minimumLength(String s) {
+        int n = s.length();
+        int left = 0, right = n - 1;
+        while (left < right && s.charAt(left) == s.charAt(right)) {
+            char c = s.charAt(left);
+            while (left <= right && s.charAt(left) == c) {
+                left++;
+            }
+            while (left <= right && s.charAt(right) == c) {
+                right--;
+            }
+        }
+        return right - left + 1;
+    }
+}
