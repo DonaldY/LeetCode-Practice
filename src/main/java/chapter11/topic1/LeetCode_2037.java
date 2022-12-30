cab4f7f4a2b3ce4052c494b2064468a050878ebc
yyf <yangyf@pingpongx.com>
2022-12-31 00:07:19 +0800

    Update: add leetcode 2037

---
+++ src/main/java/chapter11/topic1/LeetCode_2037.java
@@ -0,0 +1,21 @@
+package chapter11.topic1;
+
+import java.util.Arrays;
+
+/**
+ * @author donald
+ * @date 2022/12/31
+ */
+public class LeetCode_2037 {
+
+    // Time: O(n ^ 2), Space: O(1), Faster: 97.41%
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int res = 0;
+        for (int i = 0; i < seats.length; i++) {
+            res += Math.abs(seats[i] - students[i]);
+        }
+        return res;
+    }
+}
