166ba615e03d991fd7f9397eb51a791bb15a66c7
yyf <yangyf@pingpongx.com>
2022-12-31 00:07:19 +0800

    Update: add leetcode 2351

---
+++ src/main/java/chapter12/topic4/LeetCode_2351.java
@@ -0,0 +1,19 @@
+package chapter12.topic4;
+
+/**
+ * @author donald
+ * @date 2023/1/1
+ */
+public class LeetCode_2351 {
+    // 哈希表
+    // Time: O(n), Space: O(n), Faster: 100%
+    public char repeatedCharacter(String s) {
+        if (null == s || s.length() == 0) return 0;
+        int [] arr = new int[26];
+        for (char c : s.toCharArray()) {
+            if (arr[c - 'a'] != 0) return c;
+            ++arr[c - 'a'];
+        }
+        return 0;
+    }
+}
