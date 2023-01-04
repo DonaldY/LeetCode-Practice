a0adc70529fb7973c4cdf000f72e61d8634c1465
Donald <448641125@qq.com>
2023-01-05 00:14:58 +0800

    Update: add leetcode 1803

--- src/main/java/chapter10/topic1/LeetCode_1803.java
+++ src/main/java/chapter10/topic1/LeetCode_1803.java
@@ -8,7 +8,7 @@ public class LeetCode_1803 {
 
     // 思路一：暴力法，直接对比
     // Time: O(n^2), Space: O(1), Faster: 超时
-    public int countPairs(int[] nums, int low, int high) {
+    public int countPairsForce(int[] nums, int low, int high) {
         if (null == nums || nums.length == 0) return 0;
         int ans = 0;
         for (int i = 0; i < nums.length - 1; ++i) {
@@ -18,5 +18,71 @@ public class LeetCode_1803 {
             }
         }
         return ans;
+    }
+
+    // 方法二： 字典树
+    // 字典树的根节点
+    private Trie root = null;
+    // 最高位的二进制位编号为 14
+    private static final int HIGH_BIT = 14;
+
+    public int countPairs(int[] nums, int low, int high) {
+        return f(nums, high) - f(nums, low - 1);
+    }
+
+    public int f(int[] nums, int x) {
+        root = new Trie();
+        int res = 0;
+        for (int i = 1; i < nums.length; i++) {
+            add(nums[i - 1]);
+            res += get(nums[i], x);
+        }
+        return res;
+    }
+
+    public void add(int num) {
+        Trie cur = root;
+        for (int k = HIGH_BIT; k >= 0; k--) {
+            int bit = (num >> k) & 1;
+            if (cur.son[bit] == null) {
+                cur.son[bit] = new Trie();
+            }
+            cur = cur.son[bit];
+            cur.sum++;
+        }
+    }
+
+    public int get(int num, int x) {
+        Trie cur = root;
+        int sum = 0;
+        for (int k = HIGH_BIT; k >= 0; k--) {
+            int r = (num >> k) & 1;
+            if (((x >> k) & 1) != 0) {
+                if (cur.son[r] != null) {
+                    sum += cur.son[r].sum;
+                }
+                if (cur.son[r ^ 1] == null) {
+                    return sum;
+                }
+                cur = cur.son[r ^ 1];
+            } else {
+                if (cur.son[r] == null) {
+                    return sum;
+                }
+                cur = cur.son[r];
+            }
+        }
+        sum += cur.sum;
+        return sum;
+    }
+}
+
+class Trie {
+    // son[0] 表示左子树，son[1] 表示右子树
+    Trie[] son = new Trie[2];
+    int sum;
+
+    public Trie() {
+        sum = 0;
     }
 }
