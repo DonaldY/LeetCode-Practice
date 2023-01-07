24dbada473e358edcc67dd004e4014da13dfc109
Donald <448641125@qq.com>
2023-01-08 00:31:41 +0800

    Update: add leetcode 1658

--- src/main/java/chapter9/topic2/LeetCode_1658.java
+++ src/main/java/chapter9/topic2/LeetCode_1658.java
@@ -1,45 +1,49 @@
 package chapter9.topic2;
 
+
 /**
  * @author donald
  * @date 2023/01/07
  */
 public class LeetCode_1658 {
 
+    public static void main(String[] args) {
+
+        LeetCode_1658 leetCode_1658 = new LeetCode_1658();
+        System.out.println(leetCode_1658.minOperations(new int[]{1,1,4,2,3}, 5));
+    }
+
+    // 暴力法：DFS，一个树
     // Time: O(), Space: O(), Faster:
     public int minOperations(int[] nums, int x) {
         if (null == nums) return -1;
 
-        int ans = -1;
         int n = nums.length;
+        int ans = n + 1;
 
         int min1 = dfs(1, n - 1, nums, x - nums[0], 1);
         int min2 = dfs(0, n - 2, nums, x - nums[n - 1], 1);
-        if (min1 != -1) {
-            ans = min1;
-        }
-        if (min2 != -1) {
-            if (ans == -1) return min2;
-            else Math.min(min2, ans);
-        }
-        return ans;
+
+        ans = Math.min(Math.min(ans, min1), min2);
+
+        return ans == n + 1 ? -1 : ans;
     }
 
     private int dfs(int left, int right, int[] nums, int sum, int num) {
-        if (left > right) return -1;
-        if (0 == sum) return num;
-        if (sum < 0) return -1;
 
-        int ans = -1;
-        int min1 = dfs(left + 1, right, nums, sum - nums[left], ++num);
-        int min2 = dfs(left, right - 1, nums, sum - nums[right], ++num);
-        if (min1 != -1) {
-            ans = min1;
-        }
-        if (min2 != -1) {
-            if (ans == -1) return min2;
-            else Math.min(min2, ans);
-        }
+        if (0 == sum) return num;
+
+        int n = nums.length;
+        int ans = n + 1;
+        if (left > right) return ans;
+        if (sum < 0) return ans;
+
+
+        int min1 = dfs(left + 1, right, nums, sum - nums[left], num + 1);
+        int min2 = dfs(left, right - 1, nums, sum - nums[right], num + 1);
+
+        ans = Math.min(Math.min(ans, min1), min2);
+
         return ans;
     }
 }
