fd96a3f9c8463017de9d53cdeea4f85f2d89a26f
Donald <448641125@qq.com>
2023-01-10 23:58:04 +0800

    Update: add leetcode 753

---
+++ src/main/java/chapter4/topic4/LeetCode_753.java
@@ -0,0 +1,36 @@
+package chapter4.topic4;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author donald
+ * @date 2023/01/10
+ */
+public class LeetCode_753 {
+    Set<Integer> seen = new HashSet<>();
+    StringBuffer ans = new StringBuffer();
+    int highest;
+    int k;
+
+    public String crackSafe(int n, int k) {
+        highest = (int) Math.pow(10, n - 1);
+        this.k = k;
+        dfs(0);
+        for (int i = 1; i < n; i++) {
+            ans.append('0');
+        }
+        return ans.toString();
+    }
+
+    public void dfs(int node) {
+        for (int x = 0; x < k; ++x) {
+            int nei = node * 10 + x;
+            if (!seen.contains(nei)) {
+                seen.add(nei);
+                dfs(nei % highest);
+                ans.append(x);
+            }
+        }
+    }
+}
