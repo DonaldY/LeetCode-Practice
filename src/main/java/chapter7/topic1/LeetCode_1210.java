27e3d27b603e20b4558f67abb243ae6b7323ebdb
Donald <448641125@qq.com>
2023-02-05 21:18:26 +0800

    Update: add leetcode 1210

---
+++ src/main/java/chapter7/topic1/LeetCode_1210.java
@@ -0,0 +1,67 @@
+package chapter7.topic1;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
+/**
+ * @author donald
+ * @date 2023/02/05
+ */
+public class LeetCode_1210 {
+
+    // 广度搜索
+    // Faster: 71.79%
+    public int minimumMoves(int[][] grid) {
+        int n = grid.length;
+        int[][][] dist = new int[n][n][2];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                Arrays.fill(dist[i][j], -1);
+            }
+        }
+        dist[0][0][0] = 0;
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{0, 0, 0});
+
+        while (!queue.isEmpty()) {
+            int[] arr = queue.poll();
+            int x = arr[0], y = arr[1], status = arr[2];
+            if (status == 0) {
+                // 向右移动一个单元格
+                if (y + 2 < n && dist[x][y + 1][0] == -1 && grid[x][y + 2] == 0) {
+                    dist[x][y + 1][0] = dist[x][y][0] + 1;
+                    queue.offer(new int[]{x, y + 1, 0});
+                }
+                // 向下移动一个单元格
+                if (x + 1 < n && dist[x + 1][y][0] == -1 && grid[x + 1][y] == 0 && grid[x + 1][y + 1] == 0) {
+                    dist[x + 1][y][0] = dist[x][y][0] + 1;
+                    queue.offer(new int[]{x + 1, y, 0});
+                }
+                // 顺时针旋转 90 度
+                if (x + 1 < n && y + 1 < n && dist[x][y][1] == -1 && grid[x + 1][y] == 0 && grid[x + 1][y + 1] == 0) {
+                    dist[x][y][1] = dist[x][y][0] + 1;
+                    queue.offer(new int[]{x, y, 1});
+                }
+            } else {
+                // 向右移动一个单元格
+                if (y + 1 < n && dist[x][y + 1][1] == -1 && grid[x][y + 1] == 0 && grid[x + 1][y + 1] == 0) {
+                    dist[x][y + 1][1] = dist[x][y][1] + 1;
+                    queue.offer(new int[]{x, y + 1, 1});
+                }
+                // 向下移动一个单元格
+                if (x + 2 < n && dist[x + 1][y][1] == -1 && grid[x + 2][y] == 0) {
+                    dist[x + 1][y][1] = dist[x][y][1] + 1;
+                    queue.offer(new int[]{x + 1, y, 1});
+                }
+                // 逆时针旋转 90 度
+                if (x + 1 < n && y + 1 < n && dist[x][y][0] == -1 && grid[x][y + 1] == 0 && grid[x + 1][y + 1] == 0) {
+                    dist[x][y][0] = dist[x][y][1] + 1;
+                    queue.offer(new int[]{x, y, 0});
+                }
+            }
+        }
+
+        return dist[n - 1][n - 2][0];
+    }
+}
