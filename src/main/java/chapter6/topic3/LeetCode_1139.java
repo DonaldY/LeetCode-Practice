7fdff7a3d36a4671297d3af93ac4c8d0420f7c04
Donald <448641125@qq.com>
2023-02-17 23:33:10 +0800

    Update: add leetcode 1139

---
+++ src/main/java/chapter6/topic3/LeetCode_1139.java
@@ -0,0 +1,31 @@
+package chapter6.topic3;
+
+/**
+ * @author donald
+ * @date 2023/02/17
+ */
+public class LeetCode_1139 {
+
+    // 动态规划
+    // Time: O(m*n*min(m, n)), Space: O(m * n), Faster: 64.86%
+    public int largest1BorderedSquare(int[][] grid) {
+        int m = grid.length, n = grid[0].length;
+        int[][] left = new int[m + 1][n + 1];
+        int[][] up = new int[m + 1][n + 1];
+        int maxBorder = 0;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (grid[i - 1][j - 1] == 1) {
+                    left[i][j] = left[i][j - 1] + 1;
+                    up[i][j] = up[i - 1][j] + 1;
+                    int border = Math.min(left[i][j], up[i][j]);
+                    while (left[i - border + 1][j] < border || up[i][j - border + 1] < border) {
+                        border--;
+                    }
+                    maxBorder = Math.max(maxBorder, border);
+                }
+            }
+        }
+        return maxBorder * maxBorder;
+    }
+}
