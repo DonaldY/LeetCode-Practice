97442daff18967ff7db139fbda2aa5579244906c
Donald <448641125@qq.com>
2023-03-19 00:48:40 +0800

    Update: add leetcode 2390

--- src/main/java/chapter12/topic4/LeetCode_2390.java
+++ src/main/java/chapter12/topic4/LeetCode_2390.java
@@ -1,8 +1,36 @@
 package chapter12.topic4;
 
+import java.util.Arrays;
+
 /**
  * @author donald
  * @date 2023/03/19
  */
 public class LeetCode_2390 {
+    public int[] answerQueries(int[] nums, int[] queries) {
+        int n = nums.length, m = queries.length;
+        Arrays.sort(nums);
+        int[] f = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            f[i + 1] = f[i] + nums[i];
+        }
+        int[] answer = new int[m];
+        for (int i = 0; i < m; i++) {
+            answer[i] = binarySearch(f, queries[i]) - 1;
+        }
+        return answer;
+    }
+
+    public int binarySearch(int[] f, int target) {
+        int low = 1, high = f.length;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (f[mid] > target) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
 }
