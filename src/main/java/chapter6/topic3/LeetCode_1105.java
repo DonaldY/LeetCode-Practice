33c307a256682de91e25e2a49bcd58185543960b
Donald <448641125@qq.com>
2023-04-23 23:49:08 +0800

    Update: add leetcode 1105

---
+++ src/main/java/chapter6/topic3/LeetCode_1105.java
@@ -0,0 +1,29 @@
+package chapter6.topic3;
+
+import java.util.Arrays;
+
+/**
+ * @author donald
+ * @date 2023/04/23
+ */
+public class LeetCode_1105 {
+
+    public int minHeightShelves(int[][] books, int shelfWidth) {
+        int n = books.length;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, 1000000);
+        dp[0] = 0;
+        for (int i = 0; i < n; ++i) {
+            int maxHeight = 0, curWidth = 0;
+            for (int j = i; j >= 0; --j) {
+                curWidth += books[j][0];
+                if (curWidth > shelfWidth) {
+                    break;
+                }
+                maxHeight = Math.max(maxHeight, books[j][1]);
+                dp[i + 1] = Math.min(dp[i + 1], dp[j] + maxHeight);
+            }
+        }
+        return dp[n];
+    }
+}
